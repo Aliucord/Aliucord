9ec3918ddae0338daa31ff470e20b0dc58eac638
Juby210 <juby210@vp.pl>
2021-05-20 18:28:22 +0200

    [skip ci] make constants final

--- Aliucord/src/main/java/com/aliucord/Constants.java
+++ Aliucord/src/main/java/com/aliucord/Constants.java
@@ -11,27 +11,27 @@ import com.discord.stores.StoreClientVersion;
 import com.discord.stores.StoreStream;
 
 @SuppressWarnings("unused")
-public class Constants {
+public final class Constants {
     // Font resource ids, they're not defined by any generated package but they seem to be constant so i made this class.
-    public static class Fonts {
+    public static final class Fonts {
         private static final int base = 0x7f090000;
 
-        public static int ginto_bold = base;
-        public static int ginto_medium = base + 1;
-        public static int ginto_regular = base + 2;
-        public static int roboto_medium_numbers = base + 3;
-        public static int sourcecodepro_semibold = base + 4;
-        public static int whitney_bold = base + 5;
-        public static int whitney_medium = base + 6;
-        public static int whitney_semibold = base + 7;
+        public static final int ginto_bold = base;
+        public static final int ginto_medium = base + 1;
+        public static final int ginto_regular = base + 2;
+        public static final int roboto_medium_numbers = base + 3;
+        public static final int sourcecodepro_semibold = base + 4;
+        public static final int whitney_bold = base + 5;
+        public static final int whitney_medium = base + 6;
+        public static final int whitney_semibold = base + 7;
     }
 
-    public static String ALIUCORD_SUPPORT = "EsNDvBaHVU";
+    public static final String ALIUCORD_SUPPORT = "EsNDvBaHVU";
 
-    public static String BASE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Aliucord";
+    public static final String BASE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Aliucord";
 
-    public static String NAMESPACE_ANDROID = "http://schemas.android.com/apk/res/android";
-    public static String NAMESPACE_APP = "http://schemas.android.com/apk/res-auto";
+    public static final String NAMESPACE_ANDROID = "http://schemas.android.com/apk/res/android";
+    public static final String NAMESPACE_APP = "http://schemas.android.com/apk/res-auto";
 
     public static int DISCORD_VERSION;
 
