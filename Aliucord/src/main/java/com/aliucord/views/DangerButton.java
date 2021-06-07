a5fdba33037251fe7dd522c2a957293b2805121d
X1nto <burtkni@yandex.ru>
2021-06-07 23:02:35 +0400

    Added documentation for Button and DangerButton

--- Aliucord/src/main/java/com/aliucord/views/DangerButton.java
+++ Aliucord/src/main/java/com/aliucord/views/DangerButton.java
@@ -9,6 +9,10 @@ import com.lytefast.flexinput.R$h;
 
 public class DangerButton extends MaterialButton {
 
+    /**
+     * Creates a red Discord styled button.
+     * @param context {@link Context}
+     */
     public DangerButton(Context context) {
         super(new ContextThemeWrapper(context, R$h.UiKit_Material_Button_Red), null, 0);
     }
