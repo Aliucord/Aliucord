4fc6c921697e8399281ba04c17ec625461203d2b
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-06-12 22:32:12 +0200

    restore get comments in stub for easier reading code

--- DiscordStubs/src/main/java/com/discord/api/emoji/GuildEmoji.java
+++ DiscordStubs/src/main/java/com/discord/api/emoji/GuildEmoji.java
@@ -10,36 +10,43 @@ import java.util.List;
 public final class GuildEmoji {
     public GuildEmoji(long id, String name, List<Long> roles, boolean requireColons, boolean managed, boolean animated, Boolean available) { }
     /**
+     * isAnimated
      * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
      */
     @Deprecated
     public final boolean a() { return false; }
     /**
+     * isAvailable
      * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
      */
     @Deprecated
     public final Boolean b() { return null; }
     /**
+     * getId
      * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
      */
     @Deprecated
     public final long c() { return 0; }
     /**
+     * isManaged
      * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
      */
     @Deprecated
     public final boolean d() { return false; }
     /**
+     * getName
      * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
      */
     @Deprecated
     public final String e() { return null; }
     /**
+     * getRequireColons
      * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
      */
     @Deprecated
     public final boolean f() { return false; }
     /**
+     * getRoles
      * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
      */
     @Deprecated
