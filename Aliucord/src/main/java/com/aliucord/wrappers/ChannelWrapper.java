875d14d3017fdb2d1478124c15946adc28da4ed7
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-06-12 23:59:31 +0200

    add channel and guild wrappers, complete guild and channel stubs

---
+++ Aliucord/src/main/java/com/aliucord/wrappers/ChannelWrapper.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2021 Juby210
+ * Licensed under the Open Software License version 3.0
+ */
+
+package com.aliucord.wrappers;
+
+import com.discord.api.channel.Channel;
+import com.discord.api.channel.ChannelRecipientNick;
+import com.discord.api.user.User;
+
+import java.util.List;
+
+/**
+ * Wraps the obfuscated {@link Channel} class to provide nice method names and require only one central
+ * update if method names change after an update
+ */
+@SuppressWarnings({"unused", "deprecation"})
+public class ChannelWrapper {
+    private final Channel channel;
+
+    public ChannelWrapper(Channel channel) {
+        this.channel = channel;
+    }
+
+    public final long getApplicationId() {
+        return channel.b();
+    }
+
+    public final int getBitrate() {
+        return channel.c();
+    }
+
+    public final long getGuildId() {
+        return channel.e();
+    }
+
+    public final String getIcon() {
+        return channel.f();
+    }
+
+    public final long getId() {
+        return channel.g();
+    }
+
+    public final long getLastMessageId() {
+        return channel.h();
+    }
+
+    public final Integer getMessageCount() {
+        return channel.k();
+    }
+
+    public final String getName() {
+        return channel.l();
+    }
+
+    public final List<ChannelRecipientNick> getNicks() {
+        return channel.m();
+    }
+
+    public final boolean isNsfw() {
+        return channel.n();
+    }
+
+    public final long getParentId() {
+        return channel.q();
+    }
+
+    public final int getPosition() {
+        return channel.s();
+    }
+
+    public final int getRateLimitPerUser() {
+        return channel.t();
+    }
+
+    public final List<Long> getRecipientIds() {
+        return channel.u();
+    }
+
+    public final List<User> getRecipients() {
+        return channel.v();
+    }
+
+    public final String getRtcRegion() {
+        return channel.w();
+    }
+
+    public final String getTopic() {
+        return channel.y();
+    }
+
+    public final int getType() {
+        return channel.z();
+    }
+
+    public final int getUserLimit() {
+        return channel.A();
+    }
+}
---
+++ Aliucord/src/main/java/com/aliucord/wrappers/GuildWrapper.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2021 Juby210
+ * Licensed under the Open Software License version 3.0
+ */
+
+package com.aliucord.wrappers;
+
+import com.discord.api.channel.Channel;
+import com.discord.api.emoji.GuildEmoji;
+import com.discord.api.guild.*;
+import com.discord.api.guildmember.GuildMember;
+import com.discord.api.role.GuildRole;
+
+import java.util.List;
+
+/**
+ * Wraps the obfuscated {@link Guild} class to provide nice method names and require only one central
+ * update if method names change after an update
+ */
+@SuppressWarnings({"unused", "deprecation"})
+public class GuildWrapper {
+    private final Guild guild;
+
+    public GuildWrapper(Guild guild) {
+        this.guild = guild;
+    }
+
+    public final Long getAfkChannelId() {
+        return guild.b();
+    }
+
+    public final int getAfkTimeout() {
+        return guild.c();
+    }
+
+    public final int getApproximatePresenceCount() {
+        return guild.d();
+    }
+
+    public final String getBanner() {
+        return guild.e();
+    }
+
+    public final List<Channel> getChannelUpdates() {
+        return guild.f();
+    }
+
+    public final List<Channel> getChannels() {
+        return guild.g();
+    }
+
+    public final Integer getDefaultMessageNotifications() {
+        return guild.h();
+    }
+
+    public final String getDescription() {
+        return guild.i();
+    }
+
+    public final List<GuildEmoji> getEmojis() {
+        return guild.j();
+    }
+
+    public final GuildExplicitContentFilter getExplicitContentFilter() {
+        return guild.k();
+    }
+
+    public final List<GuildFeature> getFeatures() {
+        return guild.l();
+    }
+
+    public final String getIcon() {
+        return guild.n();
+    }
+
+    public final long getId() {
+        return guild.o();
+    }
+
+    public final String getJoinedAt() {
+        return guild.p();
+    }
+
+    public final int getApproxMemberCount() {
+        return guild.r();
+    }
+
+    public final List<GuildMember> getCachedMembers() {
+        return guild.s();
+    }
+
+    public final int getMfaLevel() {
+        return guild.t();
+    }
+
+    public final String getName() {
+        return guild.u();
+    }
+
+    public final boolean isNsfw() {
+        return guild.v();
+    }
+
+    public final long getOwnerId() {
+        return guild.w();
+    }
+
+    public final String getPreferredLocale() {
+        return guild.x();
+    }
+
+    public final int getPremiumSubscriptionCount() {
+        return guild.y();
+    }
+
+    public final int getPremiumTier() {
+        return guild.z();
+    }
+
+    public final Long getPublicUpdatesChannelId() {
+        return guild.B();
+    }
+
+    public final String getRegion() {
+        return guild.C();
+    }
+
+    public final List<GuildRole> getRoles() {
+        return guild.D();
+    }
+
+    public final Long getRulesChannelId() {
+        return guild.E();
+    }
+
+    public final String getSplash() {
+        return guild.F();
+    }
+
+    public final int getSystemChannelFlags() {
+        return guild.H();
+    }
+
+    public final Long getSystemChannelId() {
+        return guild.I();
+    }
+
+    public final List<Channel> getThreads() {
+        return guild.J();
+    }
+
+    public final boolean isUnavailable() {
+        return guild.K();
+    }
+
+    public final String getVanityUrlCode() {
+        return guild.L();
+    }
+
+    public final GuildVerificationLevel getVerificationLevel() {
+        return guild.M();
+    }
+}
