94b984aa7b5848c5b3144316d374c46e35640a1f
Vendicated <vendicated@riseup.net>
2021-06-12 03:50:33 +0200

    hmlfdfdhhjj

--- DiscordStubs/src/main/java/com/discord/api/emoji/GuildEmoji.java
+++ DiscordStubs/src/main/java/com/discord/api/emoji/GuildEmoji.java
@@ -4,24 +4,44 @@ import java.util.List;
 
 /**
  * Obfuscated class with regularly changing method names.
- * Do not use this directly, use Aliucords wrapper instead
- * @see com.aliucord.wrappers.GuildEmojiWrapper
+ * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
  */
 @SuppressWarnings("unused")
 public final class GuildEmoji {
     public GuildEmoji(long id, String name, List<Long> roles, boolean requireColons, boolean managed, boolean animated, Boolean available) { }
+    /**
+     * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
+     */
     @Deprecated
     public final boolean a() { return false; }
+    /**
+     * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
+     */
     @Deprecated
     public final Boolean b() { return null; }
+    /**
+     * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
+     */
     @Deprecated
     public final long c() { return 0; }
+    /**
+     * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
+     */
     @Deprecated
     public final boolean d() { return false; }
+    /**
+     * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
+     */
     @Deprecated
     public final String e() { return null; }
+    /**
+     * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
+     */
     @Deprecated
     public final boolean f() { return false; }
+    /**
+     * Do not use this directly, use {@link com.aliucord.wrappers.GuildEmojiWrapper} instead
+     */
     @Deprecated
     public final List<Long> g() { return null; }
 }
