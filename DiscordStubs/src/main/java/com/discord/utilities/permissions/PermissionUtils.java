52bfc256f53be4d52eb4f6ef2465d912e8c08844
Vendicated <vendicated@riseup.net>
2021-06-17 02:17:06 +0200

    Fix license headers

--- DiscordStubs/src/main/java/com/discord/utilities/permissions/PermissionUtils.java
+++ DiscordStubs/src/main/java/com/discord/utilities/permissions/PermissionUtils.java
@@ -1,13 +1,3 @@
-/*
- * Ven's Aliucord Plugins
- * Copyright (C) 2021 Vendicated
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- */
-
 package com.discord.utilities.permissions;
 
 import androidx.annotation.NonNull;
