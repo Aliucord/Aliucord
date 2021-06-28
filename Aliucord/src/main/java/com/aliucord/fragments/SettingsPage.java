855f1e5bcdf9b2d1182b7795af29d8da06bd6fc5
Vendicated <vendicated@riseup.net>
2021-06-29 01:31:01 +0200

    Add helper methods to SettingsPage and BottomSheet

--- Aliucord/src/main/java/com/aliucord/fragments/SettingsPage.java
+++ Aliucord/src/main/java/com/aliucord/fragments/SettingsPage.java
@@ -6,29 +6,99 @@
 package com.aliucord.fragments;
 
 import android.annotation.SuppressLint;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.widget.NestedScrollView;
 
 import com.aliucord.Utils;
 import com.discord.app.AppFragment;
+import com.google.android.material.appbar.AppBarLayout;
 
+@SuppressWarnings("unused")
 public class SettingsPage extends AppFragment {
+    private static final int resId = Utils.getResId("widget_settings_behavior", "layout");
+    private CoordinatorLayout view;
+    private LinearLayout layout;
+    private Toolbar toolbar;
+
     @SuppressLint("ResourceType")
-    public SettingsPage() { super(Utils.getResId("widget_settings_behavior", "layout")); }
+    public SettingsPage() {
+        super(resId);
+    }
 
     @Override
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public void onViewBound(View view) {
         super.onViewBound(view);
+        this.view = (CoordinatorLayout) view;
+
         setActionBarSubtitle("Aliucord");
         setActionBarDisplayHomeAsUpEnabled();
 
-        // Clear page layout
-        ((LinearLayout) ((NestedScrollView) ((CoordinatorLayout) view).getChildAt(1)).getChildAt(0)).removeAllViews();
+        clear();
+        setPadding(Utils.getDefaultPadding());
+    }
+
+    /** Returns the LinearLayout associated with this Page */
+    public final LinearLayout getLinearLayout() {
+        if (layout == null) {
+            if (view == null) throw new IllegalStateException("This Page has not been initialised yet. Did you forget to call super.onViewBound?");
+            layout = (LinearLayout) ((NestedScrollView) view.getChildAt(1)).getChildAt(0);
+        }
+        return layout;
+    }
+
+    /** Returns the Toolbar associated with this Page */
+    public final Toolbar getHeaderBar() {
+        if (toolbar == null) {
+            if (view == null) throw new IllegalStateException("This Page has not been initialised yet. Did you forget to call super.onViewBound?");
+            toolbar = (Toolbar) ((AppBarLayout) view.getChildAt(0)).getChildAt(0);
+        }
+        return toolbar;
+    }
+
+    /** Sets the padding of the LinearLayout associated with this Page */
+    public final void setPadding(int p) {
+        getLinearLayout().setPadding(p, p, p, p);
+    }
+
+    /** Removes a button from the Toolbar associated with this Page */
+    public final void addHeaderButton(AppCompatButton button) {
+        getHeaderBar().addView(button);
+    }
+
+    /** Adds a button to the Toolbar associated with this Page */
+    public final void removeHeaderButton(AppCompatButton button) {
+        getHeaderBar().removeView(button);
+    }
+
+    /** Adds a view to the LinearLayout associated with this Page */
+    public final void addView(View view) {
+        getLinearLayout().addView(view);
+    }
+
+    /** Removes a view from the LinearLayout associated with this Page */
+    public final void removeView(View view) {
+        getLinearLayout().removeView(view);
+    }
+
+    /** Removes all views from the LinearLayout associated with this Page */
+    public final void clear() {
+        getLinearLayout().removeAllViews();
+    }
+
+    /** Removes all views from the LinearLayout associated with this Page and calls onViewBound */
+    public final void reRender() {
+        clear();
+        onViewBound(requireView());
+    }
+
+    /** Closes this SettingsPage by simulating a back press */
+    public final void close() {
+        requireActivity().onBackPressed();
     }
 }
--- Aliucord/src/main/java/com/aliucord/widgets/BottomSheet.java
+++ Aliucord/src/main/java/com/aliucord/widgets/BottomSheet.java
@@ -11,12 +11,15 @@ import android.widget.LinearLayout;
 
 import androidx.core.widget.NestedScrollView;
 
+import com.aliucord.Utils;
 import com.discord.app.AppBottomSheet;
 import com.discord.widgets.channels.WidgetChannelSelector;
 
 @SuppressWarnings("unused")
 public class BottomSheet extends AppBottomSheet {
     private static int id = 0;
+    private NestedScrollView view;
+    private LinearLayout layout;
 
     @Override
     public int getContentViewResId() {
@@ -28,7 +31,38 @@ public class BottomSheet extends AppBottomSheet {
     public void onViewCreated(View view, Bundle bundle) {
         super.onViewCreated(view, bundle);
 
-        // Clear bottom sheet layout
-        ((LinearLayout) ((NestedScrollView) view).getChildAt(0)).removeAllViews();
+        this.view = (NestedScrollView) view;
+
+        clear();
+        setPadding(Utils.getDefaultPadding());
+    }
+
+    /** Returns the LinearLayout associated with this BottomSheet */
+    public LinearLayout getLinearLayout() {
+        if (layout == null) {
+            if (view == null) throw new IllegalStateException("This BottomSheet has not been initialised yet. Did you forget to call super.onViewCreated?");
+            layout = (LinearLayout) view.getChildAt(0);
+        }
+        return layout;
+    }
+
+    /** Sets the padding of the LinearLayout associated with this BottomSheet */
+    public final void setPadding(int p) {
+        getLinearLayout().setPadding(p, p, p, p);
+    }
+
+    /** Removes all views of the LinearLayout associated with this BottomSheet */
+    public void clear() {
+        getLinearLayout().removeAllViews();
+    }
+
+    /** Adds a view to the LinearLayout associated with this BottomSheet */
+    public final void addView(View view) {
+        getLinearLayout().addView(view);
+    }
+
+    /** Removes a view from the LinearLayout associated with this BottomSheet */
+    public final void removeView(View view) {
+        getLinearLayout().removeView(view);
     }
 }
