863165161d1b873359fc7a3d9dd5f3e43b0e2fa1
Nicholas Owens <nwowens32@gmail.com>
2021-07-12 15:56:42 -0400

    Update StoreUserSettings stub (#54)

--- DiscordStubs/src/main/java/com/discord/stores/StoreUserSettings.java
+++ DiscordStubs/src/main/java/com/discord/stores/StoreUserSettings.java
@@ -1,10 +1,24 @@
 package com.discord.stores;
 
-import android.content.SharedPreferences;
+import android.content.Context;
+import com.discord.app.AppActivity;
+import com.discord.models.domain.ModelGuildFolder;
+import java.util.Collection;
+import java.util.List;
+import rx.Observable;
 
 @SuppressWarnings("unused")
-public class StoreUserSettings {
-    public SharedPreferences prefs = null;
+public final class StoreUserSettings {
+    public static final Companion Companion = new Companion();
+    public static final class Companion {}
+
+    public final void expireCustomStatus() {}
+
+    public static Observable observeIsAnimatedEmojisEnabled$default(StoreUserSettings storeUserSettings, boolean z2, int i, Object obj) { return null; }
+    public static Observable observeIsAutoPlayGifsEnabled$default(StoreUserSettings storeUserSettings, boolean z2, int i, Object obj) { return null; }
+    public static Observable observeStickerAnimationSettings$default(StoreUserSettings storeUserSettings, boolean z2, int i, Object obj) { return null; }
+
+    public final void updateAllowAccessibilityDetectionInternal(boolean z2) {}
 
     public final boolean getIsAttachmentMediaInline() { return false; }
     public final boolean getIsAutoImageCompressionEnabled() { return false; }
@@ -19,10 +33,39 @@ public class StoreUserSettings {
     public final boolean getIsSyncTextAndImagesEnabled() { return false; }
     public final int getStickerAnimationSettings() { return 0; }
 
-    public final void setIsAutoImageCompressionEnabled(boolean z2) { }
+    public void init(Context context) {}
+
+    public final Observable<Integer> observeExplicitContentFilter() { return null; }
+    public final Observable<Integer> observeFriendDiscoveryFlags() { return null; }
+    public final Observable<List<ModelGuildFolder>> observeGuildFolders() { return null; }
+    public final Observable<Boolean> observeIsAccessibilityDetectionAllowed() { return null; }
+    public final Observable<Boolean> observeIsAnimatedEmojisEnabled(boolean z2) { return null; }
+    public final Observable<Boolean> observeIsAutoPlayGifsEnabled(boolean z2) { return null; }
+    public final Observable<Boolean> observeIsDefaultGuildsRestricted() { return null; }
+    public final Observable<Boolean> observeIsRenderEmbedsEnabled() { return null; }
+    public final Observable<Boolean> observeIsShowCurrentGameEnabled() { return null; }
+    public final Observable<Boolean> observeIsStickerSuggestionsEnabled() { return null; }
+    public final Observable<List<Long>> observeRestrictedGuildIds() { return null; }
+    public final Observable<Integer> observeStickerAnimationSettings(boolean z2) { return null; }
+
+    public final void setDefaultGuildsRestricted(AppActivity appActivity, boolean z2, Collection<Long> collection) {}
+    public final void setExplicitContentFilter(AppActivity appActivity, int i) {}
+    public final void setFriendDiscoveryFlags(AppActivity appActivity, int i) {}
+    public final void setFriendSourceFlags(AppActivity appActivity, Boolean bool, Boolean bool2, Boolean bool3) {}
+    public final void setIsAnimatedEmojisEnabled(AppActivity appActivity, boolean z2) {}
+    public final void setIsAttachmentMediaInline(AppActivity appActivity, boolean z2) {}
+    public final void setIsAutoImageCompressionEnabled(boolean z2) {}
     public final boolean setIsAutoPlayGifsEnabled(boolean z2) { return false; }
-    public final void setIsChromeCustomTabsEnabled(boolean z2) { }
-    public final void setIsMobileOverlayEnabled(boolean z2) { }
-    public final void setIsShiftEnterToSendEnabled(boolean z2) { }
+    public final void setIsChromeCustomTabsEnabled(boolean z2) {}
+    public final void setIsDeveloperMode(AppActivity appActivity, boolean z2) {}
+    public final void setIsEmbedMediaInlined(AppActivity appActivity, boolean z2) {}
+    public final void setIsMobileOverlayEnabled(boolean z2) {}
+    public final void setIsRenderEmbedsEnabled(AppActivity appActivity, boolean z2) {}
+    public final void setIsShiftEnterToSendEnabled(boolean z2) {}
+    public final void setIsShowCurrentGameEnabled(AppActivity appActivity, boolean z2) {}
     public final boolean setIsStickerSuggestionsEnabled(boolean z2) { return false; }
+    public final void setRestrictedGuildId(AppActivity appActivity, long j, boolean z2) {}
+    public final void setRestrictedGuildIds(AppActivity appActivity, Collection<Long> collection, long j, boolean z2) {}
+    public final void setStickerAnimationSettings(AppActivity appActivity, int i) {}
+    public final void getIsSyncTextAndImagesEnabled(boolean z2) {}
 }
