548251387887462605c7fc1fa8964cb60db2c8ed
Nicholas Owens <nwowens32@gmail.com>
2021-08-11 07:26:13 -0400

    Add `plugins` command (#71)
    * Create StoreUserSettings.java
    
    * Update CoreCommands.java
    
    * Show plugin count
    
    * Forgot to import Set

--- Aliucord/src/main/java/com/aliucord/coreplugins/CoreCommands.java
+++ Aliucord/src/main/java/com/aliucord/coreplugins/CoreCommands.java
@@ -6,13 +6,16 @@
 package com.aliucord.coreplugins;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 
+import com.aliucord.PluginManager;
 import com.aliucord.api.CommandsAPI;
 import com.aliucord.entities.Plugin;
 
 import java.util.Collections;
+import java.util.Set;
 
 final class CoreCommands extends Plugin {
     @NonNull
@@ -33,6 +36,18 @@ final class CoreCommands extends Plugin {
                 "Sends message",
                 Collections.singletonList(CommandsAPI.requiredMessageOption),
                 ctx -> new CommandsAPI.CommandResult(ctx.getRequiredString("message"))
+        );
+
+        commands.registerCommand(
+                "plugins",
+                "Lists installed plugins",
+                Collections.emptyList(),
+                ctx -> {
+                    Set<String> plugins = PluginManager.plugins.keySet();
+                    if (plugins.isEmpty()) return new CommandsAPI.CommandResult("No plugins installed", null, false);
+                    String content = String.format("Installed Plugins (%s): %s", plugins.size(), TextUtils.join(", ", plugins));
+                    return new CommandsAPI.CommandResult(content, null, false);
+                }
         );
     }
 
