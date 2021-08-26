1ab4dfa6e669b2b57085c8530934a85e71d92f1b
js6pak <kubastaron@hotmail.com>
2021-08-26 21:06:35 +0200

    Add kdoc for show

--- Aliucord/src/main/java/com/aliucord/utils/ChangelogUtils.kt
+++ Aliucord/src/main/java/com/aliucord/utils/ChangelogUtils.kt
@@ -10,6 +10,15 @@ import com.aliucord.Utils
 import com.discord.widgets.changelog.WidgetChangeLog
 
 object ChangelogUtils {
+    /**
+     * Show ChangeLog modal
+     *
+     * @param context Context
+     * @param version Version shown in the header
+     * @param media Url to an image or a video that is displayed above body, null for nothing
+     * @param body Changelog text in markdown (with custom discord rules)
+     * @param footerActions Icons shown in the footer
+     */
     @JvmStatic
     fun show(
         context: Context,
