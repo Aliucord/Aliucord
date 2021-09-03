4d6c42677b7156633f8bad017fb611e8ec86144f
Vendicated <vendicated@riseup.net>
2021-09-03 18:15:04 +0200

    Add doc

--- Aliucord/src/main/java/com/aliucord/annotations/AliucordPlugin.kt
+++ Aliucord/src/main/java/com/aliucord/annotations/AliucordPlugin.kt
@@ -1,13 +1,33 @@
+/*
+ * This file is part of Aliucord, an Android Discord client mod.
+ * Copyright (c) 2021 Juby210 & Vendicated
+ * Licensed under the Open Software License version 3.0
+ */
+
 package com.aliucord.annotations
 
 /**
- * Annotates the entrypoint of a plugin, used by manifest.json generation
+ * Annotates the entrypoint of a plugin, used for the manifest.json generation
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
+@Suppress("unused")
 annotation class AliucordPlugin(
+    /**
+     * The version code of your plugin, e.g. 1.0.0
+     * Used by the updater to figure out if plugins are outdated.
+     */
     val version: String = "",
+    /**
+     * The description of your plugin, will show on the plugin card in Settings > Plugins
+     */
     val description: String = "",
+    /**
+     * Your plugin's changelog. Supports markdown
+     */
     val changelog: String = "",
+    /**
+     * The banner that will show on top of the changelog screen
+     */
     val changelogMedia: String = ""
 )
