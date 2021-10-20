3b14b25f2fa760cead9c5fd407ac7be8628fc96e
litleck <nwowens32@gmail.com>
2021-10-20 18:35:56 -0400

    Added DP extension to DimenUtils

--- Aliucord/src/main/java/com/aliucord/utils/DimenUtils.kt
+++ Aliucord/src/main/java/com/aliucord/utils/DimenUtils.kt
@@ -3,58 +3,55 @@
  * Copyright (c) 2021 Juby210
  * Licensed under the Open Software License version 3.0
  */
+package com.aliucord.utils
 
-package com.aliucord.utils;
+import com.aliucord.Utils.appContext
+import com.discord.utilities.dimen.DimenUtils
 
-import com.aliucord.Utils;
+object DimenUtils {
+    private val density = appContext.resources.displayMetrics.density
 
-public final class DimenUtils {
-    private static final float density = Utils.getAppContext().getResources().getDisplayMetrics().density;
+    /**
+     * The default padding for items (16 DP)
+     */
+    @JvmStatic
+    val defaultPadding = dpToPx(16)
+
+    /**
+     * The default padding for cards (8 DP)
+     */
+    @JvmStatic
+    val defaultCardRadius = dpToPx(8)
+
+    /**
+     * Converts DP to PX
+     * @return `DP` converted to PX
+     */
+    val Int.dp: Int
+        get() = dpToPx(this)
 
     /**
      * Converts DP to PX.
      * @param dp DP value
-     * @return <code>DP</code> converted to PX
+     * @return `DP` converted to PX
      */
-    public static int dpToPx(int dp) { return dpToPx((float) dp); }
+    @JvmStatic
+    fun dpToPx(dp: Int): Int = dpToPx(dp.toFloat())
 
     /**
      * Converts DP to PX.
      * @param dp DP value
-     * @return <code>DP</code> converted to PX
+     * @return `DP` converted to PX
      */
-    public static int dpToPx(float dp) { return (int) (dp * density + 0.5f); }
+    @JvmStatic
+    fun dpToPx(dp: Float): Int = (dp * density + 0.5f).toInt()
 
     /**
      * Converts PX to DP.
      * @param px PX value
-     * @return <code>PX</code> converted to DP
-     * @see com.discord.utilities.dimen.DimenUtils#pixelsToDp(int) 
+     * @return `PX` converted to DP
+     * @see com.discord.utilities.dimen.DimenUtils.pixelsToDp
      */
-    public static int pxToDp(int px) { return com.discord.utilities.dimen.DimenUtils.pixelsToDp(px); }
-
-    private static int defaultPadding = 0;
-    private static int defaultCardRadius = 0;
-
-    /**
-     * Gets the default padding for the items. (16 DP)
-     * @return default padding
-     * @see DimenUtils#dpToPx(int)
-     * @see DimenUtils#dpToPx(float)
-     */
-    public static int getDefaultPadding() {
-        if (defaultPadding == 0) defaultPadding = dpToPx(16);
-        return defaultPadding;
-    }
-
-    /**
-     * Gets the default radius for cards. (8 DP)
-     * @return default padding
-     * @see DimenUtils#dpToPx(int)
-     * @see DimenUtils#dpToPx(float)
-     */
-    public static int getDefaultCardRadius() {
-        if (defaultCardRadius == 0) defaultCardRadius = dpToPx(8);
-        return defaultCardRadius;
-    }
+    @JvmStatic
+    fun pxToDp(px: Int): Int = DimenUtils.pixelsToDp(px)
 }
