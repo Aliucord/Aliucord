088b9bc22048209453aea3a75ad7a89336fe9bc8
Manti <67705577+mantikafasi@users.noreply.github.com>
2021-11-08 21:57:45 +0300

    updated RWrapper

---
+++ Aliucord/src/main/java/com/aliucord/wrappers/R.java
@@ -0,0 +1,13 @@
+package com.aliucord.wrappers;
+
+public class R {
+    public static com.lytefast.flexinput.R.a animator;
+    public static com.lytefast.flexinput.R.b attrs;
+    public static com.lytefast.flexinput.R.c colors;
+    public static com.lytefast.flexinput.R.d dimens;
+    public static com.lytefast.flexinput.R.e drawables;
+    public static com.lytefast.flexinput.R.f id;
+    public static com.lytefast.flexinput.R.g layout;
+    public static com.lytefast.flexinput.R.h strings;
+    public static com.lytefast.flexinput.R.i styles;
+}
