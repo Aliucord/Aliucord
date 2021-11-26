cb68b265f6113fe9dc64566b33979cd34359ea7b
Marvin Witt <contact@nurmarv.in>
2021-11-26 15:57:50 +0100

    Port to 104.3 (#158)

--- Aliucord/src/main/java/com/aliucord/Utils.kt
+++ Aliucord/src/main/java/com/aliucord/Utils.kt
@@ -359,7 +359,7 @@ Consider installing the MiXplorer file manager, or navigate to $path manually us
             -1,
             name ?: "Clyde",
             NullSerializable.b(avatarUrl ?: Constants.Icons.CLYDE),
-            NullSerializable.a(),
+            null,
             "0000",
             0,
             null,
@@ -376,8 +376,9 @@ Consider installing the MiXplorer file manager, or navigate to $path manually us
             null,
             null,
             null,
-            NullSerializable.a(),
-            null
+            null,
+            null,
+            0,
         )
 
     /**
