386d6c4cecfdd10b80a023937b388052f69e9a09
TymanWasTaken <tyman@tyman.tech>
2021-11-28 16:19:58 -0500

    Syntax and suppress warning

--- Aliucord/src/main/java/com/aliucord/settings/SettingsDelegates.kt
+++ Aliucord/src/main/java/com/aliucord/settings/SettingsDelegates.kt
@@ -9,13 +9,12 @@ class SettingsDelegate<T>(
     private val defaultValue: T,
     private val settings: SettingsAPI
 ) : ReadWriteProperty<Any, T> {
-    override fun getValue(thisRef: Any, property: KProperty<*>): T {
-        return settings.getUnknown(property.name, defaultValue) as T
-    }
+    @Suppress("UNCHECKED_CAST")
+    override fun getValue(thisRef: Any, property: KProperty<*>): T =
+        settings.getUnknown(property.name, defaultValue) as T
 
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) =
         settings.setUnknown(property.name, value)
-    }
 }
 
 fun <T> SettingsAPI.delegate(
