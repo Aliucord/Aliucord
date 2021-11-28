6f9862cc591fc08123cb400762e79f564df02d17
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-11-28 12:55:01 -0800

    deprecate old methods

--- Aliucord/src/main/java/com/aliucord/Logger.kt
+++ Aliucord/src/main/java/com/aliucord/Logger.kt
@@ -4,6 +4,7 @@
  */
 package com.aliucord
 
+import android.content.Context
 import android.util.Log
 import com.aliucord.Utils.showToast
 import com.discord.app.AppLog
@@ -36,6 +37,14 @@ class Logger(var module: String = "Aliucord") {
     @JvmOverloads
     fun info(msg: String, throwable: Throwable? = null) =
         AppLog.g.i(format(msg), throwable)
+
+    /**
+     * Logs a [Log.INFO] message, and shows it to the user as a toast
+     * @param msg Message to log
+     */
+    @Deprecated("Use infoToast(msg) instead", ReplaceWith("infoToast(msg)"))
+    fun info(ctx: Context?, msg: String) =
+        infoToast(msg)
 
     /**
      * Logs a [Log.INFO] message, and shows it to the user as a toast
@@ -78,12 +87,32 @@ class Logger(var module: String = "Aliucord") {
         errorToast("Sorry, something went wrong. Please try again.", throwable)
 
     /**
+     * Logs an exception and shows the user a toast saying "Sorry, something went wrong. Please try again."
+     * @param throwable Exception to log
+     */
+    @Deprecated("Use errorToast(throwable) instead", ReplaceWith("errorToast(throwable)"))
+    fun error(ctx: Context?, throwable: Throwable?) =
+        errorToast("Sorry, something went wrong. Please try again.", throwable)
+
+    /**
      * Logs a [Log.ERROR] message, shows it to the user as a toast and prints the stacktrace of the exception
      * @param msg Message to log
      * @param throwable Exception to log
      */
     @JvmOverloads
     fun errorToast(msg: String, throwable: Throwable? = null) {
+        showToast(msg, true)
+        error(msg, throwable)
+    }
+
+    /**
+     * Logs a [Log.ERROR] message, shows it to the user as a toast and prints the stacktrace of the exception
+     * @param msg Message to log
+     * @param throwable Exception to log
+     */
+    @JvmOverloads
+    @Deprecated("Use errorToast(msg, throwable) instead", ReplaceWith("errorToast(msg, throwable)"))
+    fun errorToast(ctx: Context?, msg: String, throwable: Throwable? = null) {
         showToast(msg, true)
         error(msg, throwable)
     }
