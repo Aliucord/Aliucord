7fc8538550854a53e40e280f66f81a03f3367a29
wingio <mcprowing@gmail.com>
2021-11-30 18:40:29 -0500

    Optimized imports and added javadoc

--- Aliucord/src/main/java/com/aliucord/fragments/SelectDialog.kt
+++ Aliucord/src/main/java/com/aliucord/fragments/SelectDialog.kt
@@ -1,23 +1,23 @@
 package com.aliucord.fragments
 
 import android.os.Bundle
-import android.view.*
-import android.widget.*
-import android.text.*
+import android.view.View
+import android.view.ViewGroup
+import android.view.Gravity
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.annotation.NonNull
-import androidx.annotation.Nullable
 import com.aliucord.Utils
 import com.aliucord.utils.DimenUtils
 import com.discord.app.AppDialog
 import com.lytefast.flexinput.R
 
-class SelectDialog() : AppDialog() {
+class SelectDialog() : AppDialog(Utils.getResId("widget_settings_language_select", "layout")) {
     inner class Adapter(private val items: Array<String>) : RecyclerView.Adapter<Adapter.ViewHolder?>() {
         @NonNull
         override fun onCreateViewHolder(@NonNull parent: ViewGroup, viewType: Int): ViewHolder {
             return ViewHolder(TextView(parent.getContext(), null, 0, R.i.UiKit_Settings_Item_Icon).apply {
-                setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+                layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             })
         }
 
@@ -41,14 +41,23 @@ class SelectDialog() : AppDialog() {
         }
     }
 
+    /***
+     * Called when an item is selected
+     */
     var onResultListener: ((Int) -> Unit)? = null
         set
         get
 
+    /***
+     * Items for the user to pick from
+     */
     var items: Array<String> = arrayOf()
         set
         get
 
+    /***
+     * Title displayed above the item list
+     */
     var title: String = "Title"
         set
         get
@@ -60,15 +69,15 @@ class SelectDialog() : AppDialog() {
         rv.setAdapter(Adapter(items))
         (rv.getParent() as ViewGroup).removeViewAt(0)
         val titleTv = TextView(view.getContext(), null, 0, R.i.UiKit_Sheet_Header_Title).apply {
-            setText(title)
-            setGravity(Gravity.CENTER_HORIZONTAL)
+            text = title
+            gravity = Gravity.CENTER_HORIZONTAL
             setPadding(p, p, p, p)
-            setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+            layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         }
         (rv.getParent() as ViewGroup).addView(titleTv, 0)
     }
 
-    fun onItemPicked(position: Int) {
+    private fun onItemPicked(position: Int) {
         onResultListener?.invoke(position)
         dismiss()
     }
