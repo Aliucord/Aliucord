099c76541cea4fe6c46a19391fd96a37522fc051
Diamond <33725716+DiamondMiner88@users.noreply.github.com>
2021-12-01 07:06:15 -0800

    e

--- Aliucord/src/main/java/com/aliucord/Utils.kt
+++ Aliucord/src/main/java/com/aliucord/Utils.kt
@@ -19,6 +19,7 @@ import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
 import android.text.style.ClickableSpan
 import android.text.style.StyleSpan
 import android.view.View
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
@@ -459,7 +460,11 @@ Consider installing the MiXplorer file manager, or navigate to $path manually us
             error(th)
         }
 
-        WidgetChatListAdapterItemAttachment.Companion.`access$navigateToAttachment`(WidgetChatListAdapterItemAttachment.Companion, appActivity, attachment)
+        WidgetChatListAdapterItemAttachment.Companion.`access$navigateToAttachment`(
+            WidgetChatListAdapterItemAttachment.Companion,
+            appActivity,
+            attachment
+        )
     }
 
     /**
@@ -468,12 +473,18 @@ Consider installing the MiXplorer file manager, or navigate to $path manually us
     @JvmStatic
     @JvmOverloads
     fun promptRestart(msg: String = "Restart required. Restart now?") {
+        val resources = this.appContext.resources
+        val id = resources.getIdentifier("navigation_bar_height", "dimen", "android")
+        val navbarHeight = if (id > 0) resources.getDimensionPixelSize(id) else 0
+
         val view = appActivity.findViewById<View>(android.R.id.content)
-        Snackbar.make(view, msg, Snackbar.LENGTH_INDEFINITE)
-            .setAction("Restart") { v: View ->
-                val ctx = v.context
-                val intent = ctx.packageManager.getLaunchIntentForPackage(ctx.packageName)
-                appActivity.startActivity(Intent.makeRestartActivityTask(intent!!.component))
-            }.show()
+        val bar = Snackbar.make(view, msg, Snackbar.LENGTH_INDEFINITE)
+        (bar.view.layoutParams as FrameLayout.LayoutParams).apply { topMargin = navbarHeight }
+        bar.setAction("Restart") {
+            val ctx = it.context
+            val intent = ctx.packageManager.getLaunchIntentForPackage(ctx.packageName)
+            appActivity.startActivity(Intent.makeRestartActivityTask(intent!!.component))
+        }
+        bar.show()
     }
 }
