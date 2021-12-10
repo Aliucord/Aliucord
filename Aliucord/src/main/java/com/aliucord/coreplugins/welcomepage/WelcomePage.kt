f1ba2024fa4b1eb57697729e1766a431e09778da
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2021-12-10 01:48:06 -0500

    finish the rest of it & find a better method to patch

--- Aliucord/src/main/java/com/aliucord/coreplugins/welcomepage/WelcomePage.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/welcomepage/WelcomePage.kt
@@ -7,21 +7,23 @@ import android.widget.LinearLayout.LayoutParams.MATCH_PARENT
 import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
-import com.aliucord.Constants
-import com.aliucord.Utils
 import com.lytefast.flexinput.R
 import com.aliucord.fragments.SettingsPage
 import com.aliucord.utils.DimenUtils
 import com.aliucord.views.Divider
 import com.discord.utilities.color.ColorCompat
-import com.discord.views.CheckedSetting
 import android.graphics.drawable.Drawable
 
 import android.graphics.drawable.LayerDrawable
 import android.transition.TransitionManager
+import com.aliucord.*
+import com.aliucord.coreplugins.CorePlugins
+import com.aliucord.views.Button
+import com.discord.stores.StoreInviteSettings
+import com.discord.widgets.guilds.invite.WidgetGuildInvite
 
 
-class ClosedDrawable(private val drawable: Drawable) : LayerDrawable(arrayOf(drawable)) {
+private class ClosedDrawable(private val drawable: Drawable) : LayerDrawable(arrayOf(drawable)) {
     override fun draw(canvas: Canvas) {
         val bounds = drawable.bounds
         canvas.save()
@@ -39,7 +41,7 @@ class WelcomePage : SettingsPage() {
         super.onViewBound(view)
 
         val ctx = requireContext()
-        /*view.findViewById<ViewGroup>(Utils.getResId("action_bar_toolbar", "id")).visibility = View.GONE*/
+        view.findViewById<ViewGroup>(Utils.getResId("action_bar_toolbar", "id")).visibility = View.GONE
         setPadding(0)
 
         val layout = LinearLayout(ctx).apply {
@@ -57,8 +59,11 @@ class WelcomePage : SettingsPage() {
 
         val closed = ClosedDrawable(open!!)
 
-        with(layout) {
+        val warning = ContextCompat.getDrawable(ctx, R.e.ic_warning_circle_24dp)?.apply {
+            mutate()
+        }
 
+        with(layout) {
             TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).run {
                 text = "Welcome to Aliucord"
                 typeface = ResourcesCompat.getFont(context, Constants.Fonts.whitney_bold)
@@ -66,6 +71,7 @@ class WelcomePage : SettingsPage() {
                 gravity = Gravity.CENTER
                 addView(this)
             }
+
             TextView(ctx, null, 0, R.i.UiKit_Settings_Item_SubText).run {
                 text = "Tap a topic below to learn more about it"
                 typeface = ResourcesCompat.getFont(context, Constants.Fonts.whitney_semibold)
@@ -76,35 +82,214 @@ class WelcomePage : SettingsPage() {
 
             addView(Divider(ctx).apply { scaleY = 5f })
 
-            // break the news to clueless people coming from youtube tutorials LMAO
 
             val fromYT = LinearLayout(ctx).apply {
-                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply { text = "dsfjiknbuhszfjknhsdzfjoiuzsdjio" })
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply {
+                    gravity = Gravity.CENTER
+                    text = "If you were promised \"free nitro\" or anything like that, you have been LIED TO."
+                    textSize = 16f
+                    compoundDrawablePadding = DimenUtils.dpToPx(4)
+                    setTextColor(ColorCompat.getColor(ctx, R.c.premium_perk_orange))
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(warning, null, null, null)
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT).apply {
+                        orientation = LinearLayout.VERTICAL
+                    }
+                })
+
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_SubText).apply {
+                    gravity = Gravity.CENTER
+                    text = """
+                    • Plugin Downloader is built in. There is no need to install it separately.
+                    • Plugins for PC client mods DO NOT WORK on Aliucord, duh.
+                    • Support Server: The development, plugin-request discussion, and offtopic channels are NOT support channels.
+                """.trimIndent()
+                    textSize = 16f
+                })
+                dividerPadding = DimenUtils.defaultPadding
+                gravity = Gravity.CENTER
+                visibility = View.GONE
             }
 
             TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).run {
-                text = "IF YOU CAME FROM A YOUTUBE TUTORIAL"
+                text = "READ THIS FIRST!"
                 gravity = Gravity.CENTER
                 compoundDrawablePadding = DimenUtils.dpToPx(4)
-                setCompoundDrawablesRelativeWithIntrinsicBounds(open, null, null, null)
+                setCompoundDrawablesRelativeWithIntrinsicBounds(closed, null, null, null)
                 typeface = ResourcesCompat.getFont(ctx, Constants.Fonts.whitney_bold)
                 setTextColor(ColorCompat.getColor(ctx, R.c.uikit_btn_bg_color_selector_red))
                 textSize = 16f
                 setOnClickListener {
                     TransitionManager.beginDelayedTransition(fromYT)
                     fromYT.visibility = if (fromYT.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(if (fromYT.visibility == View.VISIBLE) open else closed, null, null, null)
                 }
                 addView(this)
             }
             addView(fromYT)
+            addView(Divider(ctx))
 
-            // how to find & install plugins - explain that plugin downloader is built in
+            val installPlugins = LinearLayout(ctx).apply {
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_SubText).apply {
+                    gravity = Gravity.CENTER
+                    text = "PluginDownloader is built in! You don't need to do anything extra."
+                    textSize = 16f
+                    compoundDrawablePadding = DimenUtils.dpToPx(4)
+                    setTextColor(ColorCompat.getColor(ctx, R.c.premium_perk_orange))
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(warning, null, null, null)
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT).apply {
+                        orientation = LinearLayout.VERTICAL
+                    }
+                })
 
-            // how to install themes
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_SubText).apply {
+                    gravity = Gravity.CENTER
+                    text = """
+                        0. Join the Aliucord Server below.
+                        1. Navigate to #plugins-list
+                        2. Long-press (hold down on) a message until a context menu pops up
+                        3. Press "Open PluginDownloader", then install a plugin from there.
+                        3.5. Most plugins require a restart to fully start working, so you may see a prompt to restart.
+                """.trimIndent()
+                    textSize = 16f
+                })
+                dividerPadding = DimenUtils.defaultPadding
+                gravity = Gravity.CENTER
+                visibility = View.GONE
+            }
 
-            // explain how to request a plugin
+            TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).run {
+                text = "How to install Plugins"
+                gravity = Gravity.CENTER
+                compoundDrawablePadding = DimenUtils.dpToPx(4)
+                setCompoundDrawablesRelativeWithIntrinsicBounds(closed, null, null, null)
+                typeface = ResourcesCompat.getFont(ctx, Constants.Fonts.whitney_bold)
+                setTextColor(ColorCompat.getColor(ctx, R.c.brand_new))
+                textSize = 16f
+                setOnClickListener {
+                    TransitionManager.beginDelayedTransition(installPlugins)
+                    installPlugins.visibility = if (installPlugins.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(if (installPlugins.visibility == View.VISIBLE) open else closed, null, null, null)
+                }
+                addView(this)
+            }
+            addView(installPlugins)
+            addView(Divider(ctx))
+
+
+            val installThemes = LinearLayout(ctx).apply {
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_SubText).apply {
+                    gravity = Gravity.CENTER
+                    text =
+                        "The Themer plugin is required to download and use themes. Follow the \"How to install plugins\" guide if you don't know how to install it."
+                    textSize = 16f
+                    compoundDrawablePadding = DimenUtils.dpToPx(4)
+                    setTextColor(ColorCompat.getColor(ctx, R.c.premium_perk_orange))
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(warning, null, null, null)
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT).apply {
+                        orientation = LinearLayout.VERTICAL
+                    }
+                })
+
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_SubText).apply {
+                    gravity = Gravity.CENTER
+                    text = """
+                        0. Join the Aliucord Server below.
+                        1. Navigate to #themes
+                        2. Long-press (hold down on) a message until a context menu pops up
+                        3. Press "Install [Theme Name]".
+                        4. Enable it from Themer settings, if not already enabled.
+                        4.5. You may want to enable transparency, custom fonts, or custom sounds for the best effect.
+                """.trimIndent()
+                    textSize = 16f
+                })
+                dividerPadding = DimenUtils.defaultPadding
+                gravity = Gravity.CENTER
+                visibility = View.GONE
+            }
+
+            TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).run {
+                text = "How to install Themes"
+                gravity = Gravity.CENTER
+                compoundDrawablePadding = DimenUtils.dpToPx(4)
+                setCompoundDrawablesRelativeWithIntrinsicBounds(closed, null, null, null)
+                typeface = ResourcesCompat.getFont(ctx, Constants.Fonts.whitney_bold)
+                setTextColor(ColorCompat.getColor(ctx, R.c.brand_new))
+                textSize = 16f
+                setOnClickListener {
+                    TransitionManager.beginDelayedTransition(installThemes)
+                    installThemes.visibility = if (installThemes.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(if (installThemes.visibility == View.VISIBLE) open else closed, null, null, null)
+                }
+                addView(this)
+            }
+            addView(installThemes)
+            addView(Divider(ctx))
+
+            val requestPlugin = LinearLayout(ctx).apply {
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).apply {
+                    gravity = Gravity.CENTER
+                    isAllCaps = false
+                    text = "Please search in #plugin-list to make sure the plugin doesn't already exist first."
+                    textSize = 16f
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT).apply {
+                        orientation = LinearLayout.VERTICAL
+                    }
+                    setTextColor(ColorCompat.getColor(ctx, R.c.premium_perk_orange))
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(warning, null, null, null)
+                })
+
+                addView(TextView(ctx, null, 0, R.i.UiKit_Settings_Item_SubText).apply {
+                    gravity = Gravity.CENTER
+                    text = """
+                        0. Join the Aliucord Server below.
+                        1. Navigate to #plugin-request-discussion (look for something named closely, the name likely changed)
+                        2. Go to the pinned messages and READ THEM!
+                        3. Follow the instructions in the pinned message & create a plugin request on the GitHub.
+                        USE SEARCH BEFORE REQUESTING so you don't make a duplicate request!
+                """.trimIndent()
+                    textSize = 16f
+                })
+                dividerPadding = DimenUtils.defaultPadding
+                gravity = Gravity.CENTER
+                visibility = View.GONE
+            }
+
+            TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Header).run {
+                text = "Requesting a NEW plugin"
+                gravity = Gravity.CENTER
+                compoundDrawablePadding = DimenUtils.dpToPx(4)
+                setCompoundDrawablesRelativeWithIntrinsicBounds(closed, null, null, null)
+                typeface = ResourcesCompat.getFont(ctx, Constants.Fonts.whitney_bold)
+                setTextColor(ColorCompat.getColor(ctx, R.c.brand_new))
+                textSize = 16f
+                setOnClickListener {
+                    TransitionManager.beginDelayedTransition(requestPlugin)
+                    requestPlugin.visibility = if (requestPlugin.visibility == View.VISIBLE) View.GONE else View.VISIBLE
+                    setCompoundDrawablesRelativeWithIntrinsicBounds(if (requestPlugin.visibility == View.VISIBLE) open else closed, null, null, null)
+                }
+                addView(this)
+            }
+            addView(requestPlugin)
+            addView(Divider(ctx).apply { scaleY = 5f })
+
+            addView(Button(ctx).apply {
+                text = "Aliucord Support Server"
+                isAllCaps = false
+                setOnClickListener { (WidgetGuildInvite.Companion).launch(ctx, StoreInviteSettings.InviteCode(Constants.ALIUCORD_SUPPORT, "", null)) }
+            })
+
+            addView(Button(ctx).apply {
+                text = "I understand, close this!"
+                setOnClickListener {
+                    close()
+                    PluginManager.plugins["WelcomePage"]?.settings?.setBool("hasShownWelcome", true)
+                }
+            })
         }
-
         addView(layout)
+        setOnBackPressed {
+            PluginManager.plugins["WelcomePage"]?.settings?.setBool("hasShownWelcome", true)
+            return@setOnBackPressed false
+        }
     }
 }
--- Aliucord/src/main/java/com/aliucord/coreplugins/welcomepage/WelcomeScreen.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/welcomepage/WelcomeScreen.kt
@@ -3,9 +3,12 @@ package com.aliucord.coreplugins.welcomepage
 import android.content.Context
 import com.aliucord.Constants
 import com.aliucord.Utils
+import com.aliucord.api.PatcherAPI
+import com.aliucord.api.SettingsAPI
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.Hook
 import com.aliucord.patcher.Patcher
+import com.discord.models.domain.auth.ModelLoginResult
 import com.discord.stores.StoreAuthentication
 import com.discord.stores.StoreStream
 
@@ -19,12 +22,11 @@ internal class WelcomeScreen : Plugin() {
     }
 
     override fun start(context: Context) {
-        Patcher.addPatch(StoreAuthentication::class.java.getDeclaredMethod("handleAuthToken$${Constants.RELEASE_SUFFIX}", String::class.java), Hook {
-            if (!StoreStream.getAuthentication().isAuthed && settings.getBool("showGuides", false)) return@Hook
+        Patcher.addPatch(StoreAuthentication::class.java.getDeclaredMethod("handleLoginResult", ModelLoginResult::class.java), Hook {
+            if (settings.getBool("hasShownWelcome", false)) return@Hook
+            Utils.openPageWithProxy(context, WelcomePage())
         })
-        Utils.openPageWithProxy(context, WelcomePage())
     }
-
 
     override fun stop(context: Context) {}
 }
