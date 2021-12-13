eca26ac2fcb53bf3be1f51d5319c4f9d1c87771e
wingio <mcprowing@gmail.com>
2021-12-12 19:09:10 -0500

    Make requested changes

--- Aliucord/src/main/java/com/aliucord/fragments/SelectDialog.kt
+++ Aliucord/src/main/java/com/aliucord/fragments/SelectDialog.kt
@@ -12,11 +12,14 @@ import com.aliucord.utils.DimenUtils
 import com.discord.app.AppDialog
 import com.lytefast.flexinput.R
 
-class SelectDialog() : AppDialog(Utils.getResId("widget_settings_language_select", "layout")) {
-    inner class Adapter(private val items: Array<String>) : RecyclerView.Adapter<Adapter.ViewHolder?>() {
+/**
+ * Creates a dialog similar to the language picker, allows you to supply a list of options for a user to select from.
+ */
+class SelectDialog() : AppDialog(Utils.getResId("widget_settings_language_select", "layout")!!) {
+    private inner class Adapter(private val items: Array<String>) : RecyclerView.Adapter<Adapter.ViewHolder?>() {
         @NonNull
         override fun onCreateViewHolder(@NonNull parent: ViewGroup, viewType: Int): ViewHolder {
-            return ViewHolder(TextView(parent.getContext(), null, 0, R.i.UiKit_Settings_Item_Icon).apply {
+            return ViewHolder(TextView(parent.context, null, 0, R.i.UiKit_Settings_Item_Icon).apply {
                 layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
             })
         }
@@ -58,23 +61,23 @@ class SelectDialog() : AppDialog(Utils.getResId("widget_settings_language_select
     /***
      * Title displayed above the item list
      */
-    var title: String = "Title"
+    var title: String = "Select an item"
         set
         get
 
     override fun onViewBound(view: View) {
         super.onViewBound(view)
-        val p: Int = DimenUtils.dpToPx(16)
-        val rv: RecyclerView = view.findViewById(Utils.getResId("settings_language_select_list", "id")) as RecyclerView
+        val p = DimenUtils.dpToPx(16)
+        val rv = view.findViewById(Utils.getResId("settings_language_select_list", "id")) as RecyclerView
         rv.setAdapter(Adapter(items))
-        (rv.getParent() as ViewGroup).removeViewAt(0)
+        (rv.parent as ViewGroup).removeViewAt(0)
         val titleTv = TextView(view.getContext(), null, 0, R.i.UiKit_Sheet_Header_Title).apply {
             text = title
             gravity = Gravity.CENTER_HORIZONTAL
-            setPadding(p, p, p, p)
             layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            setPadding(p, p, p, p)
         }
-        (rv.getParent() as ViewGroup).addView(titleTv, 0)
+        (rv.parent as ViewGroup).addView(titleTv, 0)
     }
 
     private fun onItemPicked(position: Int) {
