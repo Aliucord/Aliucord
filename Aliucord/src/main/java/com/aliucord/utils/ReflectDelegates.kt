6342da270f96c0a9d98b337e5baf83473199833d
wingio <mcprowing@gmail.com>
2022-02-05 21:39:06 -0500

    Add documentation to LazyField

--- Aliucord/src/main/java/com/aliucord/utils/ReflectDelegates.kt
+++ Aliucord/src/main/java/com/aliucord/utils/ReflectDelegates.kt
@@ -9,12 +9,23 @@ import java.lang.reflect.*
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-class LazyField<T>(private val clazz: Class<*>, private val field: String?) : ReadOnlyProperty<T, Field> {
+/**
+ * A lazy field delegate designed to improve the performance in field reflection.
+ *
+ * @param clazz     The class that the field belongs to.
+ * @param fieldName The name of the field.
+ */
+class LazyField<T>(private val clazz: Class<*>, private val fieldName: String?) : ReadOnlyProperty<T, Field> {
     private var v = null as Field?
-    override fun getValue(thisRef: T, property: KProperty<*>) = v ?: clazz.getDeclaredField(field ?: property.name.replace("Field", "")).apply {
+    override fun getValue(thisRef: T, property: KProperty<*>) = v ?: clazz.getDeclaredField(fieldName ?: property.name.replace("Field", "")).apply {
         setAccessible(true)
         v = this
     }
 }
 
-inline fun <reified T> lazyField(field: String? = null) = LazyField<Any>(T::class.java, field)
+/**
+ * A lazy field delegate designed to improve the performance in field reflection.
+ *
+ * @param fieldName The name of the field.
+ */
+inline fun <reified T> lazyField(fieldName: String? = null) = LazyField<Any>(T::class.java, field)
