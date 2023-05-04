5dfda84d9354ca1bd2b736cccb95e5c0c403bd75
zt64 <nwowens32@gmail.com>
2023-05-04 18:47:22 -0400

    Refactor project

--- Aliucord/build.gradle.kts
+++ Aliucord/build.gradle.kts
@@ -1,8 +1,7 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
-    id("com.android.library")
     id("maven-publish")
-    id("com.aliucord.gradle")
-    id("kotlin-android")
     id("org.jetbrains.dokka")
 }
 
@@ -23,31 +22,9 @@ aliucord {
 }
 
 android {
-    compileSdk = 30
-
     defaultConfig {
-        minSdk = 24
-        targetSdk = 30
-
         buildConfigField("String", "GIT_REVISION", "\"${getGitHash()}\"")
-        buildConfigField("int", "DISCORD_VERSION", findProperty("discord_version") as String)
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-        freeCompilerArgs = freeCompilerArgs +
-            "-Xno-call-assertions" +
-            "-Xno-param-assertions" +
-            "-Xno-receiver-assertions"
+        buildConfigField("int", "DISCORD_VERSION", libs.versions.discord.get())
     }
 
     buildFeatures {
@@ -55,30 +32,50 @@ android {
     }
 }
 
-
 dependencies {
-    api("androidx.appcompat:appcompat:1.4.1")
-    api("com.google.android.material:material:1.5.0")
-    api("androidx.constraintlayout:constraintlayout:2.1.3")
-
-    discord("com.discord:discord:${findProperty("discord_version")}")
-    api("com.aliucord:Aliuhook:fb7bf41")
+    api(libs.appcompat)
+    api(libs.material)
+    api(libs.constraintlayout)
+    api(libs.aliuhook)
 }
 
-tasks.dokkaHtml.configure {
-    dokkaSourceSets {
-        named("main") {
-            noAndroidSdkLink.set(false)
-            includeNonPublic.set(false)
+tasks {
+    dokkaHtml.configure {
+        dokkaSourceSets {
+            named("main") {
+                noAndroidSdkLink.set(false)
+                includeNonPublic.set(false)
+            }
         }
     }
-}
 
-tasks.dokkaJavadoc.configure {
-    dokkaSourceSets {
-        named("main") {
-            noAndroidSdkLink.set(false)
-            includeNonPublic.set(false)
+    dokkaJavadoc.configure {
+        dokkaSourceSets {
+            named("main") {
+                noAndroidSdkLink.set(false)
+                includeNonPublic.set(false)
+            }
+        }
+    }
+
+    create("pushDebuggable") {
+        group = "aliucord"
+
+        val aliucordPath = "/storage/emulated/0/Aliucord/"
+
+        doLast {
+            exec {
+                commandLine(android.adbExecutable, "shell", "touch", "$aliucordPath.debuggable")
+            }
+
+            exec {
+                commandLine(
+                    android.adbExecutable,
+                    "push",
+                    rootProject.file(".assets/AndroidManifest-debuggable.xml"),
+                    "${aliucordPath}AndroidManifest.xml"
+                )
+            }
         }
     }
 }
@@ -86,7 +83,7 @@ tasks.dokkaJavadoc.configure {
 afterEvaluate {
     publishing {
         publications {
-            register(project.name, MavenPublication::class) {
+            register<MavenPublication>(project.name) {
                 from(components["debug"])
                 artifact(tasks["debugSourcesJar"])
             }
@@ -107,27 +104,6 @@ afterEvaluate {
             } else {
                 mavenLocal()
             }
-        }
-    }
-}
-
-task("pushDebuggable") {
-    group = "aliucord"
-
-    val aliucordPath = "/storage/emulated/0/Aliucord/"
-
-    doLast {
-        exec {
-            commandLine(android.adbExecutable, "shell", "touch", "$aliucordPath.debuggable")
-        }
-
-        exec {
-            commandLine(
-                android.adbExecutable,
-                "push",
-                rootProject.file(".assets/AndroidManifest-debuggable.xml"),
-                aliucordPath + "AndroidManifest.xml"
-            )
         }
     }
 }
