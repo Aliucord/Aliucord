4ede37d19f7deb0a34e10c426a4817e95bf47366
Nick <nwowens32@gmail.com>
2023-05-08 11:02:19 -0400

    [skip ci] refactor project (#349)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,70 @@
-buildscript {
+@file:Suppress("UnstableApiUsage")
+
+import com.aliucord.gradle.AliucordExtension
+import com.android.build.gradle.BaseExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlin) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.aliucord.gradle) apply false
+    alias(libs.plugins.dokka) apply false
+}
+
+subprojects {
+    apply {
+        plugin("com.android.library")
+        plugin("kotlin-android")
+        plugin("com.aliucord.gradle")
+    }
+
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
         maven("https://maven.aliucord.com/snapshots")
     }
+
+    android {
+        namespace = "com.aliucord"
+
+        compileSdkVersion(30)
+
+        @Suppress("ExpiredTargetSdkVersion")
+        defaultConfig {
+            minSdk = 24
+            targetSdk = 30
+        }
+
+        buildTypes {
+            get("release").isMinifyEnabled = false
+        }
+
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
+    }
+
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("com.aliucord:gradle:bbcd8a8")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
+        val discord by configurations
+
+        discord(rootProject.libs.discord)
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+            freeCompilerArgs = freeCompilerArgs +
+                "-Xno-call-assertions" +
+                "-Xno-param-assertions" +
+                "-Xno-receiver-assertions"
+        }
     }
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-        maven("https://maven.aliucord.com/snapshots")
-    }
-}
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
 
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
-}
+fun Project.aliucord(configuration: AliucordExtension.() -> Unit) =
+    extensions.getByName<AliucordExtension>("aliucord").configuration()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,12 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        maven("https://jitpack.io")
+        maven("https://maven.aliucord.com/snapshots")
+    }
+}
+
 include(":Aliucord")
 include(":Injector")
 rootProject.name = "Aliucord"
