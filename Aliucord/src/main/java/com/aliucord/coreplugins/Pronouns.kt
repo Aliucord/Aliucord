a8e16cb52aee5bd2a0826ec69851f88cffa10135
Ty <ty@blahaj.land>
2023-09-12 22:46:31 -0600

    Remove redundant variable (thisObject is already cast and put in scope as `this`)

--- Aliucord/src/main/java/com/aliucord/coreplugins/Pronouns.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/Pronouns.kt
@@ -24,15 +24,14 @@ internal class Pronouns : Plugin(Manifest("Pronouns")) {
     override fun load(context: Context) {
         patcher.after<UserProfileHeaderView>("configureSecondaryName", UserProfileHeaderViewModel.ViewState.Loaded::class.java) {
             val state = it.args[0] as? UserProfileHeaderViewModel.ViewState.Loaded ?: return@after
-            val view = it.thisObject as UserProfileHeaderView
 
             val profile = state.userProfile as? RNUserProfile ?: return@after
             val pronouns = profile.guildMemberProfile?.pronouns?.ifEmpty { null }
                 ?: profile.userProfile?.pronouns?.ifEmpty { null }
                 ?: return@after
 
-            if (view.id == sheetProfileHeaderViewId) {
-                val secondaryNameView = view.findViewById<SimpleDraweeSpanTextView>(userProfileHeaderSecondaryNameViewId)
+            if (id == sheetProfileHeaderViewId) {
+                val secondaryNameView = findViewById<SimpleDraweeSpanTextView>(userProfileHeaderSecondaryNameViewId)
                 val layout = secondaryNameView.parent as LinearLayout
 
                 layout.findViewById(pronounsViewId) ?: TextView(layout.context, null, 0, com.lytefast.flexinput.R.i.UiKit_TextView_Semibold).apply {
