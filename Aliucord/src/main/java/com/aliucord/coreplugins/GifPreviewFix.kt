4d95cccd0807782e5fa8779aef2159342df86704
Kyiro <54126666+Kyiro@users.noreply.github.com>
2023-10-17 21:42:11 +0200

    GifPreviewFix coreplugin

---
+++ Aliucord/src/main/java/com/aliucord/coreplugins/GifPreviewFix.kt
@@ -0,0 +1,28 @@
+package com.aliucord.coreplugins
+
+import android.content.Context
+import android.net.Uri
+import com.aliucord.entities.Plugin
+import com.aliucord.patcher.after
+import com.discord.utilities.embed.EmbedResourceUtils
+
+internal class GifPreviewFix : Plugin(Manifest("GifPreviewFix")) {
+    override fun load(context: Context) {
+        patcher.after<EmbedResourceUtils>("getPreviewUrls", String::class.java, Int::class.java, Int::class.java, Boolean::class.java) {
+            // return List<String>
+            var result = (it.result as List<String>).toMutableList()
+
+            val uri = Uri.parse(result[0])
+            if (uri.path?.endsWith(".gif") == true) {
+                val newUri = uri.buildUpon().encodedQuery("format=gif").build()
+                result[0] = newUri.toString()
+
+                it.result = result
+            }
+        }
+    }
+
+    override fun start(context: Context?) {}
+
+    override fun stop(context: Context?) {}
+}
