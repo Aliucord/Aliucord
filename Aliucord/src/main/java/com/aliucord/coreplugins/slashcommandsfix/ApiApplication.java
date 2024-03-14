75a60360e7705b4a714444b9093580f5802360a4
Grzesiek11 <grzesiek11@stary.pc.pl>
2024-03-14 18:32:54 +0100

    Prefer bot user name and icon over application name and icon

--- Aliucord/src/main/java/com/aliucord/coreplugins/slashcommandsfix/ApiApplication.java
+++ Aliucord/src/main/java/com/aliucord/coreplugins/slashcommandsfix/ApiApplication.java
@@ -6,17 +6,23 @@
 
 package com.aliucord.coreplugins.slashcommandsfix;
 
+import com.discord.models.user.User;
+import com.discord.stores.StoreStream;
+import java.util.Optional;
+
 class ApiApplication {
     public final long id;
     public final String name;
     public final String icon;
     public final ApiPermissions permissions;
+    public final Long botId;
 
     public ApiApplication() {
         this.id = 0;
         this.name = null;
         this.icon = null;
         this.permissions = null;
+        this.botId = null;
     }
 
     public Application toModel(int commandCount) {
@@ -26,6 +32,8 @@ class ApiApplication {
         } else {
             permissions = new Permissions(null, null, null);
         }
-        return new Application(this.id, this.name, this.icon, permissions, commandCount);
+        var usersStore = StoreStream.getUsers();
+        Optional<User> botUser = Optional.ofNullable(this.botId).map(userId -> usersStore.getUsers().get(userId));
+        return new Application(this.id, this.name, this.icon, permissions, commandCount, botUser);
     }
 }
--- Aliucord/src/main/java/com/aliucord/coreplugins/slashcommandsfix/Application.java
+++ Aliucord/src/main/java/com/aliucord/coreplugins/slashcommandsfix/Application.java
@@ -6,11 +6,16 @@
 
 package com.aliucord.coreplugins.slashcommandsfix;
 
+import com.aliucord.Logger;
+import com.discord.models.user.User;
+import com.discord.utilities.user.UserUtils;
+import java.util.Optional;
+
 class Application extends com.discord.models.commands.Application {
     public Permissions permissions_;
 
-    public Application(long id, String name, String icon, Permissions permissions, int commandCount) {
-        super(id, name, icon, null, commandCount, null, false);
+    public Application(long id, String name, String icon, Permissions permissions, int commandCount, Optional<User> botUser) {
+        super(id, name, icon, null, commandCount, botUser.map(user -> UserUtils.INSTANCE.synthesizeApiUser(user)).orElse(null), false);
         this.permissions_ = permissions;
     }
 }
