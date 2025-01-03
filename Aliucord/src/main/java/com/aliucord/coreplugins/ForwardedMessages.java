c22eed46eea02b6a418489e484a0adaa7d46945d
mantikafasi <mantikafasi@gmail.com>
2025-01-03 22:04:33 +0300

    add jump to source

--- Aliucord/src/main/java/com/aliucord/coreplugins/ForwardedMessages.java
+++ Aliucord/src/main/java/com/aliucord/coreplugins/ForwardedMessages.java
@@ -2,14 +2,25 @@ package com.aliucord.coreplugins;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
+import androidx.core.widget.NestedScrollView;
+
+import com.aliucord.Utils;
 import com.aliucord.entities.CorePlugin;
 import com.aliucord.patcher.Hook;
 import com.aliucord.patcher.PreHook;
 import com.aliucord.utils.ReflectUtils;
 import com.discord.api.message.Message;
+import com.discord.api.permission.Permission;
+import com.discord.stores.StoreStream;
+import com.discord.utilities.PermissionOverwriteUtilsKt;
+import com.discord.utilities.color.ColorCompat;
+import com.discord.widgets.chat.list.actions.WidgetChatListActions;
 import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage;
 
 import java.util.ArrayList;
@@ -45,6 +56,7 @@ public class ForwardedMessages extends CorePlugin {
                 var snapshots = (ArrayList<Object>) ReflectUtils.getField(cf.args[0], "messageSnapshots");
                 if (snapshots == null || snapshots.isEmpty()) return;
                 var tw = (TextView) ReflectUtils.getField(cf.thisObject, "itemTag");
+                if (tw == null) return;
                 tw.setVisibility(View.VISIBLE);
                 tw.setText("FORWARDED");
                 cf.setResult(null);
@@ -52,6 +64,45 @@ public class ForwardedMessages extends CorePlugin {
                 logger.error(e);
             }
         }));
+
+
+        var viewId = View.generateViewId();
+        Drawable icon = ContextCompat.getDrawable(context, com.lytefast.flexinput.R.e.ic_reply_24dp).mutate();
+        icon.setTint(ColorCompat.getThemedColor(Utils.appActivity, com.lytefast.flexinput.R.b.colorInteractiveNormal));
+
+        patcher.patch(WidgetChatListActions.class.getDeclaredMethod("configureUI", WidgetChatListActions.Model.class),
+            new Hook((cf) -> {
+                var actions = (WidgetChatListActions) cf.thisObject;
+                var message = ((WidgetChatListActions.Model) cf.args[0]).getMessage();
+
+                try {
+                    ArrayList<Object> snapshots = (ArrayList<Object>) ReflectUtils.getField(message, "messageSnapshots");
+                    if (snapshots == null || snapshots.isEmpty()) return;
+                    var reference = message.getMessageReference();
+                    var permissionOverwrites = StoreStream.getChannels().getChannel(reference.a()).v();
+
+                    for (var permissionOverwrite : permissionOverwrites) {
+                        if (PermissionOverwriteUtilsKt.denies(permissionOverwrite, Permission.VIEW_CHANNEL)) {
+                            return;
+                        }
+                    }
+                    var scrollView = (NestedScrollView) actions.getView();
+                    var lay = (LinearLayout) scrollView.getChildAt(0);
+                    if (lay.findViewById(viewId) == null) {
+                        TextView tw = new TextView(lay.getContext(), null, 0, com.lytefast.flexinput.R.i.UiKit_Settings_Item_Icon);
+                        tw.setId(viewId);
+                        tw.setText("Jump to source");
+                        tw.setCompoundDrawablesRelativeWithIntrinsicBounds(icon, null, null, null);
+                        lay.addView(tw, 8);
+                        tw.setOnClickListener((v) -> {
+                            StoreStream.getMessagesLoader().jumpToMessage(reference.a(), reference.c());
+                            actions.dismiss();
+                        });
+                    }
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    logger.error(e);
+                }
+            }));
     }
 
     @Override
