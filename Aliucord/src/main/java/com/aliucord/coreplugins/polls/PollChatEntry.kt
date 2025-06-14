c9a11ec5c44ef1d28a4cbdd33e886dbed0d11b92
LavaDesu <me@lava.moe>
2025-06-14 13:43:51 +1000

    style(Polls): use kt single-expression functions

--- Aliucord/src/main/java/com/aliucord/coreplugins/polls/PollChatEntry.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/polls/PollChatEntry.kt
@@ -5,15 +5,10 @@ import com.discord.models.message.Message
 import com.discord.widgets.chat.list.entries.ChatListEntry
 
 internal class PollChatEntry(val poll: MessagePoll, val message: Message) : ChatListEntry() {
-    override fun getKey(): String {
-        return "$POLL_ENTRY_TYPE-${message.id}"
-    }
-
-    override fun getType(): Int {
-        return POLL_ENTRY_TYPE
-    }
-
     companion object {
         const val POLL_ENTRY_TYPE = 101 // This is just a random number I picked - Lava (lavadesu)
     }
+
+    override fun getKey() = "$POLL_ENTRY_TYPE-${message.id}"
+    override fun getType() = POLL_ENTRY_TYPE
 }
