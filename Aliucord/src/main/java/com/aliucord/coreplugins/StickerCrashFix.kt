2d0f1d9dcb8ab9b63c18609f391b3df45e2e75c8
LavaDesu <me@lava.moe>
2025-06-22 16:26:44 +1000

    fix: avoid crash upon rendering certain stickers

---
+++ Aliucord/src/main/java/com/aliucord/coreplugins/StickerCrashFix.kt
@@ -0,0 +1,29 @@
+package com.aliucord.coreplugins
+
+import android.content.Context
+import com.aliucord.entities.CorePlugin
+import com.aliucord.patcher.before
+import com.linecorp.apng.decoder.Apng
+
+internal class StickerCrashFix : CorePlugin(Manifest("StickerCrashFix")) {
+    override val isHidden = true
+    override val isRequired = true
+
+    override fun load(context: Context) {
+        patcher.before<Apng>(
+            Integer::class.javaPrimitiveType!!,
+            Integer::class.javaPrimitiveType!!,
+            Integer::class.javaPrimitiveType!!,
+            Integer::class.javaPrimitiveType!!,
+            IntArray::class.java,
+            Integer::class.javaPrimitiveType!!,
+            Long::class.javaPrimitiveType!!,
+        ) { param ->
+            val durations = param.args[4] as IntArray
+            param.args[4] = durations.map { it.coerceAtLeast(100) }.toIntArray()
+        }
+    }
+
+    override fun start(context: Context) {}
+    override fun stop(context: Context) {}
+}
