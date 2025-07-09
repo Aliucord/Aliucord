5efb59d34262f8fc347ad81d523eb70d9496a55a
LavaDesu <me@lava.moe>
2025-07-10 00:46:23 +1000

    refactor: move nonce generation into Utils

--- Aliucord/src/main/java/com/aliucord/coreplugins/polls/creation/PollCreatePayload.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/polls/creation/PollCreatePayload.kt
@@ -1,7 +1,7 @@
 package com.aliucord.coreplugins.polls.creation
 
+import com.aliucord.Utils
 import com.discord.api.message.poll.MessagePoll
-import com.discord.utilities.SnowflakeUtils
 import kotlin.random.Random
 
 @Suppress("unused", "PrivatePropertyName")
@@ -9,9 +9,7 @@ internal data class PollCreatePayload(private val poll: MessagePoll) {
     private val mobile_network_type = "wifi"
     private val signal_strength = Random.nextInt(1, 4) // TODO: Use real values maybe?
     private val content = ""
-    // For nonce, there is NonceGenerator, but it seems to use time in the future. RN and Desktop doesn't do this,
-    // so it also wasn't used here. Instead we just generate a random long and add it with current time
-    private val nonce = SnowflakeUtils.fromTimestamp(System.currentTimeMillis()) + Random.nextBits(23)
+    private val nonce = Utils.generateRNNonce()
     private val tts = false
     private val flags = 0
 }
