cf27cb8395e1fa8f19ddfa8486438bc8a4cbf739
LavaDesu <me@lava.moe>
2025-07-10 16:07:06 +1000

    docs: remove TODO from PollCreatePayload
    keep using random values, as accessing system APIs would likely be too much
    effort for little value

--- Aliucord/src/main/java/com/aliucord/coreplugins/polls/creation/PollCreatePayload.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/polls/creation/PollCreatePayload.kt
@@ -7,7 +7,7 @@ import kotlin.random.Random
 @Suppress("unused", "PrivatePropertyName")
 internal data class PollCreatePayload(private val poll: MessagePoll) {
     private val mobile_network_type = "wifi"
-    private val signal_strength = Random.nextInt(1, 4) // TODO: Use real values maybe?
+    private val signal_strength = Random.nextInt(1, 4)
     private val content = ""
     private val nonce = Utils.generateRNNonce()
     private val tts = false
