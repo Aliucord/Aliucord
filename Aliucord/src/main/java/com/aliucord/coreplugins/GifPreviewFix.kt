d00aefcf943e143766469a44a01856b173d55e8e
LavaDesu <me@lava.moe>
2025-07-21 22:21:05 +1000

    fix(GifPreviewFix): replace format properly

--- Aliucord/src/main/java/com/aliucord/coreplugins/GifPreviewFix.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/GifPreviewFix.kt
@@ -26,7 +26,13 @@ internal class GifPreviewFix : CorePlugin(Manifest("GifPreviewFix")) {
 
             val uri = Uri.parse(result[0])
             if (uri.path?.endsWith(".gif") == true) {
-                val newUri = uri.buildUpon().appendQueryParameter("format", "gif").build()
+                val newUri = uri
+                    .buildUpon()
+                    .encodedQuery(uri.encodedQuery?.replace(
+                        "&format=webp",
+                        "&format=gif"
+                    ))
+                    .build()
                 result[0] = newUri.toString()
 
                 it.result = result
