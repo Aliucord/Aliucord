3f2956fd14192f0f29085da87bb0cca8f7409cc3
rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>
2025-08-16 16:21:09 -0700

    cleanup

--- Aliucord/src/main/java/com/aliucord/coreplugins/AlignThreads.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/AlignThreads.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.aliucord.Utils
-import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.CorePlugin
 import com.aliucord.patcher.*
 import com.aliucord.utils.DimenUtils
@@ -20,20 +19,36 @@ internal class AlignThreads : CorePlugin(Manifest("AlignThreads")) {
     }
 
     override fun start(context: Context) {
-        patcher.after<`WidgetChannelsListItemChannelActions$binding$2`>("invoke", View::class.java) { (_, view: View) -> adjustIcon(view, true, true) }
-        patcher.after<`WidgetChatListActions$binding$2`>("invoke", View::class.java) { (_, view: View) -> adjustIcon(view, false, false) }
-    }
-
-    private fun adjustIcon(view: View, res_id: Boolean, res: Boolean) {
-        val res_id = if (res_id) "text_action_thread_browser" else "dialog_chat_actions_start_thread"
-        val id = Utils.getResId(res_id, "id")
-        val textview = view.findViewById<TextView>(id)
-        val size = DimenUtils.dpToPx(24)
-        val res = if (res) DrawableCompat.getThemedDrawableRes(textview.context, R.b.ic_thread) else R.e.ic_thread
-        val icon = ContextCompat.getDrawable(textview.context, res)!!
-        icon.setBounds(0, 0, size, size)
-        textview.setCompoundDrawables(icon, null, null, null)
+        patcher.after<`WidgetChannelsListItemChannelActions$binding$2`>("invoke", View::class.java) { (_, view: View) ->
+            adjustThreadIcon(
+                rootView = view,
+                textViewId = Utils.getResId("text_action_thread_browser", "id"),
+                themed = true,
+            )
+        }
+        patcher.after<`WidgetChatListActions$binding$2`>("invoke", View::class.java) { (_, view: View) ->
+            adjustThreadIcon(
+                rootView = view,
+                textViewId = Utils.getResId("dialog_chat_actions_start_thread", "id"),
+                themed = false,
+            )
+        }
     }
 
     override fun stop(context: Context) = patcher.unpatchAll()
+
+    private fun adjustThreadIcon(rootView: View, textViewId: Int, themed: Boolean) {
+        val iconId = if (themed) {
+            DrawableCompat.getThemedDrawableRes(rootView.context, R.b.ic_thread)
+        } else {
+            R.b.ic_thread
+        }
+        val icon = ContextCompat.getDrawable(rootView.context, iconId)!!.apply {
+            val size = DimenUtils.dpToPx(24)
+            setBounds(0, 0, size, size)
+        }
+
+        val textView = rootView.findViewById<TextView>(textViewId)
+        textView.setCompoundDrawables(icon, null, null, null)
+    }
 }
