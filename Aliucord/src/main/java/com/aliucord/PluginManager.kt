4df1874fd6a64b0a90b032060ba7133092effc40
Omar <180954465+omardotdev@users.noreply.github.com>
2025-08-17 11:10:01 +0300

    feat(CorePlugins): add AlignThreads fix (#578)
    Co-authored-by: Omar <180954465+omardotdev@users.noreply.github.com>
    Co-authored-by: rushiiMachine <33725716+rushiiMachine@users.noreply.github.com>

--- Aliucord/src/main/java/com/aliucord/PluginManager.kt
+++ Aliucord/src/main/java/com/aliucord/PluginManager.kt
@@ -292,6 +292,7 @@ object PluginManager {
             SupporterBadges(),
             TokenLogin(),
             UploadSize(),
+            AlignThreads()
         )
 
         corePlugins.forEach { p ->
