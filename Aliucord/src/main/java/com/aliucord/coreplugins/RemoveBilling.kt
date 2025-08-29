3000491c7d585e1f6fed8303fc162dcd7d6f9ee9
Omar <180954465+omardotdev@users.noreply.github.com>
2025-08-29 23:15:43 +0300

    feat: remove nitro ad from auto-compression toggle

--- Aliucord/src/main/java/com/aliucord/coreplugins/RemoveBilling.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/RemoveBilling.kt
@@ -17,6 +17,7 @@ import com.discord.widgets.settings.profile.WidgetEditProfileBannerSheet
 import b.a.a.b.d
 import b.a.a.b.c
 import com.discord.widgets.stickers.UnsendableStickerPremiumUpsellDialog
+import com.discord.widgets.settings.WidgetSettingsMedia
 
 internal class RemoveBilling : CorePlugin(Manifest("RemoveBilling")) {
     override val isHidden = true
@@ -38,12 +39,14 @@ internal class RemoveBilling : CorePlugin(Manifest("RemoveBilling")) {
             // Remove the Nitro gifting setting
             removeTextView(view, Utils.getResId("nitro_gifting", "id"))
         }
+        
         // Remove Gift button
         patcher.after<`FlexInputFragment$d`>("invoke", Any::class.java) {
 			val fragment = this.receiver as FlexInputFragment
 			val binding = fragment.j() ?: return@after
 			binding.m.visibility = View.GONE
 		}
+        
         // Remove boost/subscribe buttons on Boosting page
         patcher.after<WidgetGuildBoost>("onViewBound", View::class.java) { (_, view: View) ->
             var ids = arrayOf("boost_status_subscribe_button", "boost_status_protip", "boost_status_subscribe_button2", "view_premium_marketing_learn_more", "menu_premium_guild")
@@ -51,20 +54,28 @@ internal class RemoveBilling : CorePlugin(Manifest("RemoveBilling")) {
                 removeTextView(view, Utils.getResId(i, "id"))
             }
         }
+        
         // Remove the "Get Nitro" button from when trying to upload a custom banner
         patcher.after<d>("onViewBound", View::class.java) { (_, view: View) ->
             var view = view.findViewById<RelativeLayout>(Utils.getResId("get_premium_button", "id"))
             view.setVisibility(View.GONE)
         }
+        
         // Remove the "Get Nitro" button from when trying to use an emoji from another server
         patcher.after<c>("onViewBound", View::class.java) { (_, view: View) ->
             var view = view.findViewById<View>(Utils.getResId("premium_upsell_get_premium", "id"))
             view.setVisibility(View.GONE)
         }
+        
         // Remove the "Subscribe" button from when trying to use a sticker from another server
         patcher.after<UnsendableStickerPremiumUpsellDialog>("onViewBound", View::class.java) { (_, view: View) ->
             var view = view.findViewById<View>(Utils.getResId("sticker_premium_upsell_subscribe_button", "id"))
             view.setVisibility(View.GONE)
+        }
+
+        // Remove the Nitro advertisement on the "Auto-compress Images" toggle
+        patcher.after<WidgetSettingsMedia>("onViewBound", View::class.java) { (_, view: View) ->
+            removeTextView(view, Utils.getResId("compression_toggle_subtext", "id"))
         }
     }
 
