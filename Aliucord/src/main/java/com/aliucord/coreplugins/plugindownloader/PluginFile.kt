b260d7fc9a44974e2b0f7238a406186b093b3283
Omar <180954465+omardotdev@users.noreply.github.com>
2025-09-01 21:18:47 +0300

    fix(PluginDownloader): prevent from accidentally installing a plugin with the same name as a coreplugin

--- Aliucord/src/main/java/com/aliucord/coreplugins/plugindownloader/PluginFile.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/plugindownloader/PluginFile.kt
@@ -9,6 +9,7 @@ package com.aliucord.coreplugins.plugindownloader
 import com.aliucord.*
 import java.io.File
 import java.io.IOException
+import com.aliucord.entities.CorePlugin
 
 internal class PluginFile(val plugin: String) : File("${Constants.PLUGINS_PATH}/$plugin.zip") {
     val isInstalled
@@ -21,7 +22,11 @@ internal class PluginFile(val plugin: String) : File("${Constants.PLUGINS_PATH}/
     fun install(url: String, callback: Runnable? = null) {
         Utils.threadPool.execute {
             val isReinstall = isInstalled
-
+            if (PluginManager.plugins[plugin] is CorePlugin) {
+                Utils.showToast("You cannot install this plugin because it has the same name as an existing coreplugin :|")
+                throw IOException()
+            }
+            
             try {
                 Http.simpleDownload(url, this)
                 // Plugins are started on the main thread.
