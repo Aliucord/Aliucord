00f19b85c91fe8e397d6814d4ec38edc3fba1483
Omar <180954465+omardotdev@users.noreply.github.com>
2025-10-04 23:01:21 +0300

    oops

--- Aliucord/src/main/java/com/aliucord/coreplugins/accountstanding/AccountStandingPage.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/accountstanding/AccountStandingPage.kt
@@ -28,8 +28,8 @@ import android.content.Context
 import com.discord.utilities.color.ColorCompat
 
 data class ApiResponse(
-        var account_standing: accountStandingState,
-        var classifications: List<userClassifications>
+    var account_standing: accountStandingState,
+    var classifications: List<userClassifications>
 ) {
     data class userClassifications(val description: String)
     data class accountStandingState(val state: Int)
@@ -38,58 +38,49 @@ data class ApiResponse(
 class AccountStandingPage : SettingsPage() {
     override fun onViewBound(view: View) {
         super.onViewBound(view)
+        // Set both the action bar and subtitle
         setActionBarTitle("Checking account standing..")
         setActionBarSubtitle("User Settings")
-
+        
         val context = view.context
 
         Utils.threadPool.execute {
+            // Fetch the safety hub/account standing data
             try {
                 val req = Http.Request.newDiscordRNRequest("/safety-hub/@me", "GET")
                 val res = req.execute()
                 val json = res.json(GsonUtils.gsonRestApi, ApiResponse::class.java)
                 val number = json.account_standing.state
-
+                var string: String?
+                // Check if theres any violations or not
                 var classificationsareEmpty = if (json.classifications.isEmpty()) true else false
-                var user_classifications =
-                        if (!classificationsareEmpty) json.classifications.first() else null
-                var violation =
-                        if (!classificationsareEmpty) user_classifications!!.description else null
-
+                var user_classifications = if (!classificationsareEmpty) json.classifications.first() else null
+                var violation = if (!classificationsareEmpty) user_classifications!!.description else null
+                // Check to see whether the account is limited, very limited, at risk or banned (or just with no violations)
+                when (number) {
+                    100 -> string = "No violations found."
+                    200 -> string = "Your account seems limited,"
+                    300 -> string = "Your account is very limited,"
+                    400 -> string = "Your account is at risk of getting banned,"
+                    500 -> string = "Your account is banned,"
+                    else -> string = "Failed to check."
+                }
+                // Replaces the previous action bar title so its no longer checking
                 Utils.mainThread.post {
                     setActionBarTitle("Account Standing")
                     // Create the progress indicator
-                    val progressContainer = createIndicator(context, number)
+                    val progressContainer = createAccountStandingIndicator(context, number)
                     linearLayout.addView(progressContainer)
                     // Creates the TextView for the user to see their account standing/status
                     TextView(context, null, 0, R.i.UiKit_Settings_Item_SubText).run {
-                        val statusText =
-                                when (number) {
-                                    100 -> "No violations found."
-                                    200 -> "Your account seems limited,"
-                                    300 -> "Your account is very limited,"
-                                    400 -> "Your account is at risk of getting banned,"
-                                    500 -> "Your account is banned,"
-                                    else -> "Failed to check."
-                                }
-                        text =
-                                if (!classificationsareEmpty) {
-                                    "$statusText You've broke the rules for $violation (there might be some more as well)."
-                                } else {
-                                    statusText
-                                }
+                        if (!classificationsareEmpty) text = string + " You've broke the rules for $violation (there might be some more aswell)." else text = string
                         typeface = ResourcesCompat.getFont(context, Constants.Fonts.whitney_medium)
                         gravity = Gravity.CENTER
-                        setPadding(
-                                DimenUtils.dpToPx(16),
-                                DimenUtils.dpToPx(16),
-                                DimenUtils.dpToPx(16),
-                                DimenUtils.dpToPx(16)
-                        )
                         linearLayout.addView(this)
                     }
                 }
             } catch (e: Exception) {
+                // Log error and set the action bar title to failed (if theres an actual error)
                 setActionBarTitle("Failed")
                 Logger("AccountStanding").warn("Failed to check account standing", e)
             }
