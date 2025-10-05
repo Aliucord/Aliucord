39b493b1e9a4aceb8dd594a607a74e08cfac08df
nyxiereal <98595166+nyxiereal@users.noreply.github.com>
2025-10-05 18:57:54 +0200

    feat: add all new discord badges automatically

---
+++ Aliucord/src/main/java/com/aliucord/coreplugins/badges/DiscordBadges.kt
@@ -0,0 +1,70 @@
+package com.aliucord.coreplugins.badges
+
+import android.content.Context
+import com.aliucord.api.rn.user.RNUserProfile
+import com.aliucord.entities.CorePlugin
+import com.aliucord.patcher.*
+import com.aliucord.utils.lazyField
+import com.discord.databinding.UserProfileHeaderBadgeBinding
+import com.discord.utilities.views.SimpleRecyclerAdapter
+import com.discord.widgets.user.Badge
+import com.discord.widgets.user.profile.UserProfileHeaderView
+import com.discord.widgets.user.profile.UserProfileHeaderViewModel
+
+internal class DiscordBadges : CorePlugin(MANIFEST) {
+    // Cached fields
+    private val f_badgesAdapter by lazyField<UserProfileHeaderView>("badgesAdapter")
+    private val f_recyclerAdapterData by lazyField<SimpleRecyclerAdapter<*, *>>("data")
+    private val f_badgeViewHolderBinding by lazyField<UserProfileHeaderView.BadgeViewHolder>("binding")
+
+    @Suppress("UNCHECKED_CAST")
+    override fun start(context: Context) {
+        // Add profile badges to the RecyclerView
+        patcher.after<UserProfileHeaderView>("updateViewState", UserProfileHeaderViewModel.ViewState.Loaded::class.java)
+        { (_, state: UserProfileHeaderViewModel.ViewState.Loaded) ->
+            val profile = state.userProfile as? RNUserProfile ?: return@after
+            val badges = profile.badges ?: return@after
+
+            // Exclude badges that are already in aliucord
+            val discordBadges = badges
+                .filter { !it.id.contains("guild_booster") &&
+                    !it.id.contains("hypesquad") &&
+                    !it.id.contains("premium") &&
+                    !it.id.contains("bug_hunter") &&
+                    !it.id.contains("verified_developer") &&
+                    !it.id.contains("staff") &&
+                    !it.id.contains("early_supporter") &&
+                    !it.id.contains("partner") }
+                .map { badgeData ->
+                    val iconUrl = "https://cdn.discordapp.com/badge-icons/${badgeData.icon}.png"
+                    Badge(0, null, badgeData.description, false, iconUrl)
+                }
+
+            val adapter = f_badgesAdapter[this] as SimpleRecyclerAdapter<Badge, UserProfileHeaderView.BadgeViewHolder>
+            val data = f_recyclerAdapterData[adapter] as MutableList<Badge>
+            data.addAll(discordBadges)
+        }
+
+        // Set image url for badge
+        patcher.after<UserProfileHeaderView.BadgeViewHolder>("bind", Badge::class.java)
+        { (_, badge: Badge) ->
+            val url = badge.objectType
+
+            // Check that badge is ours (has icon = 0 and url set)
+            if (badge.icon != 0 || url == null) return@after
+
+            val binding = f_badgeViewHolderBinding[this] as UserProfileHeaderBadgeBinding
+            val imageView = binding.b
+            imageView.setCacheableImage(url)
+        }
+    }
+
+    override fun stop(context: Context) = patcher.unpatchAll()
+
+    private companion object {
+        val MANIFEST = Manifest(
+            name = "DiscordBadges",
+            description = "Displays newly added Discord profile badges",
+        )
+    }
+}
