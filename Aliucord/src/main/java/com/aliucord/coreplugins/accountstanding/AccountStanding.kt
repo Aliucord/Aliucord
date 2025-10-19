68f1328cec28c04976957241f0cff04ddb107939
Omar <om749843@gmail.com>
2025-10-19 19:12:18 +0300

    feat: add support for notifying the user if the user has user flag HAS_UNREAD_URGENT_MESSAGES

--- Aliucord/src/main/java/com/aliucord/coreplugins/accountstanding/AccountStanding.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/accountstanding/AccountStanding.kt
@@ -13,13 +13,19 @@ import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.widget.NestedScrollView
 import com.aliucord.Utils
+import com.aliucord.Utils.openPage
+import com.aliucord.api.NotificationsAPI
 import com.aliucord.entities.CorePlugin
+import com.aliucord.entities.NotificationData
 import com.aliucord.patcher.*
+import com.aliucord.utils.MDUtils
 import com.aliucord.utils.ViewUtils.addTo
 import com.aliucord.utils.ViewUtils.findViewById
+import com.discord.stores.StoreStream
 import com.discord.widgets.settings.WidgetSettings
 import com.lytefast.flexinput.R
 
+@Suppress("MISSING_DEPENDENCY_SUPERCLASS")
 internal class AccountStanding : CorePlugin(Manifest("AccountStanding")) {
     override val isHidden = true
 
@@ -28,20 +34,32 @@ internal class AccountStanding : CorePlugin(Manifest("AccountStanding")) {
     }
 
     override fun start(context: Context) {
-       // Patches the settings menu for the authorized apps page
-       patcher.after<WidgetSettings>("onViewBound", View::class.java) { (_, view: CoordinatorLayout) ->
-           val layout = (view.getChildAt(1) as NestedScrollView).getChildAt(0) as LinearLayoutCompat
-           val baseIndex = layout.indexOfChild(layout.findViewById<TextView>("qr_scanner"))
-           val ctx = layout.context
+        // Patches the settings menu for the authorized apps page
+        patcher.after<WidgetSettings>("onViewBound", View::class.java) { (_, view: CoordinatorLayout) ->
+            val layout = (view.getChildAt(1) as NestedScrollView).getChildAt(0) as LinearLayoutCompat
+            val baseIndex = layout.indexOfChild(layout.findViewById<TextView>("qr_scanner"))
+            val ctx = layout.context
 
-           TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Icon).apply {
-               text = "Account Standing"
-               setCompoundDrawablesWithIntrinsicBounds(Utils.tintToTheme(ctx.getDrawable(R.e.ic_security_24dp)), null, null, null)
-               setOnClickListener {
-                   Utils.openPageWithProxy(ctx, AccountStandingPage())
-               }
-           }.addTo(layout, baseIndex + 1)
-       }
+            TextView(ctx, null, 0, R.i.UiKit_Settings_Item_Icon).apply {
+                text = "Account Standing"
+                setCompoundDrawablesWithIntrinsicBounds(Utils.tintToTheme(ctx.getDrawable(R.e.ic_security_24dp)), null, null, null)
+                setOnClickListener {
+                    Utils.openPageWithProxy(ctx, AccountStandingPage())
+                }
+            }.addTo(layout, baseIndex + 1)
+        }
+
+        // Checks the user's flags to determine if they have a violation or not
+        val notificationData: NotificationData = NotificationData()
+            .setTitle("Account Standing")
+            .setAutoDismissPeriodSecs(10)
+            .setOnClick { _ ->
+                openPage(Utils.appActivity, AccountStandingPage::class.java)
+                Unit.a
+            }
+            .setBody(MDUtils.render("You broke Discord's rules, Please check Account Standing for more info."))
+
+        if (StoreStream.getUsers().me.flags == 8192) NotificationsAPI.display(notificationData)
     }
 
     override fun stop(context: Context) = patcher.unpatchAll()
