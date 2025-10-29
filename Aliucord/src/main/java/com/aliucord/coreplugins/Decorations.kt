CatPieLeaf/Aliutest

feat(Decorations): implement Decorator API

Aliucord/src/main/java/com/aliucord/coreplugins/Decorations.kt
@@ -1,37 +1,55 @@
 package com.aliucord.coreplugins
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.util.AttributeSet
 import com.aliucord.coreplugins.decorations.DecorationsSettings
+import com.aliucord.coreplugins.decorations.Decorator
 import com.aliucord.entities.CorePlugin
 import com.aliucord.patcher.*
 import com.aliucord.updater.ManagerBuild
 import com.aliucord.wrappers.users.*
 import com.discord.api.user.User
+import com.discord.databinding.WidgetChannelMembersListItemUserBinding
 import com.discord.models.member.GuildMember
 import com.discord.models.user.CoreUser
 import com.discord.models.user.MeUser
 import com.discord.stores.StoreGuilds
+import com.discord.widgets.channels.list.WidgetChannelsListAdapter
+import com.discord.widgets.channels.list.items.ChannelListItem
+import com.discord.widgets.channels.list.items.ChannelListItemPrivate
+import com.discord.widgets.channels.memberlist.adapter.*
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapter
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
+import com.discord.widgets.chat.list.entries.ChatListEntry
+import com.discord.widgets.chat.list.entries.MessageEntry
+import com.discord.widgets.user.profile.UserProfileHeaderView
+import com.discord.widgets.user.profile.UserProfileHeaderViewModel
 import com.discord.api.guildmember.GuildMember as ApiGuildMember
 
 internal class Decorations : CorePlugin(Manifest().apply {
     name = "Decorations"
     description = "Adds support for various user profile decorations"
 }) {
-    // TODO: make visible once plugin is ready
-    override val isHidden = true
-
     init {
         settingsTab = SettingsTab(DecorationsSettings.Sheet::class.java, SettingsTab.Type.BOTTOM_SHEET)
     }
 
+    @SuppressLint("BuildListAdds") // remove when there's stuff to add
+    @OptIn(ExperimentalStdlibApi::class)
+    private val decorators = buildList<Decorator> {
+        // if (DecorationsSettings.enableAvatarDecoration) add(AvatarDecorator())
+    }
+
     override fun start(context: Context) {
-        if (!DecorationsSettings.enable) return
         if (!ManagerBuild.hasInjector("2.3.0") || !ManagerBuild.hasPatches("1.3.0")) {
             logger.warn("Base app outdated, cannot enable Decorations")
             return
         }
 
         patchFields()
+        patchHandlers()
+        decorators.forEach { it.patch(patcher) }
     }
 
     override fun stop(context: Context) {
@@ -89,6 +107,74 @@ internal class Decorations : CorePlugin(Manifest().apply {
 
             res.avatarDecorationData = api.avatarDecorationData
             res.collectibles = api.collectibles
+        }
+    }
+
+    private fun patchHandlers() {
+        // onDMsInit
+        patcher.after<WidgetChannelsListAdapter.ItemChannelPrivate>(
+            Int::class.javaPrimitiveType!!,
+            WidgetChannelsListAdapter::class.java,
+        ) { (_, _: Int, adapter: WidgetChannelsListAdapter) ->
+            decorators.forEach { it.onDMsInit(this, adapter) }
+        }
+
+        // onDMsConfigure
+        patcher.after<WidgetChannelsListAdapter.ItemChannelPrivate>(
+            "onConfigure",
+            Int::class.javaPrimitiveType!!,
+            ChannelListItem::class.java,
+        ) { (_, _: Int, item: ChannelListItemPrivate) ->
+            decorators.forEach { it.onDMsConfigure(this, item) }
+        }
+
+        // onMembersListInit
+        patcher.after<ChannelMembersListViewHolderMember>(
+            WidgetChannelMembersListItemUserBinding::class.java,
+        ) { (_, binding: WidgetChannelMembersListItemUserBinding) ->
+            decorators.forEach { it.onMembersListInit(this, binding) }
+        }
+
+        // onMembersListConfigure
+        patcher.after<ChannelMembersListViewHolderMember>(
+            "bind",
+            ChannelMembersListAdapter.Item.Member::class.java,
+            Function0::class.java,
+        ) { (_, member: ChannelMembersListAdapter.Item.Member, callback: `ChannelMembersListAdapter$onBindViewHolder$1`) ->
+            decorators.forEach { it.onMembersListConfigure(this, member, callback.`this$0`) }
+        }
+
+        // onMessageInit
+        patcher.after<WidgetChatListAdapterItemMessage>(
+            Int::class.javaPrimitiveType!!,
+            WidgetChatListAdapter::class.java
+        ) { (_, _: Int, adapter: WidgetChatListAdapter) ->
+            decorators.forEach { it.onMessageInit(this, adapter) }
+        }
+
+        // onMessageConfigure
+        patcher.after<WidgetChatListAdapterItemMessage>(
+            "onConfigure",
+            Int::class.javaPrimitiveType!!,
+            ChatListEntry::class.java,
+        ) { (_, _: Int, entry: MessageEntry) ->
+            decorators.forEach { it.onMessageConfigure(this, entry) }
+        }
+
+        // onProfileHeaderInit
+        patcher.after<UserProfileHeaderView>(
+            Context::class.java,
+            AttributeSet::class.java,
+        ) {
+            decorators.forEach { it.onProfileHeaderInit(this) }
+        }
+
+        // onProfileHeaderConfigure
+        patcher.after<UserProfileHeaderView>(
+            "updateViewState",
+            UserProfileHeaderViewModel.ViewState.Loaded::class.java,
+        ) { (_, state: UserProfileHeaderViewModel.ViewState.Loaded) ->
+            decorators.forEach { it.onProfileHeaderConfigure(this, state) }
         }
     }
 }

Aliucord/src/main/java/com/aliucord/coreplugins/decorations/Decorator.kt
@@ -1,0 +1,51 @@
+package com.aliucord.coreplugins.decorations
+
+import com.aliucord.api.PatcherAPI
+import com.discord.databinding.WidgetChannelMembersListItemUserBinding
+import com.discord.widgets.channels.list.WidgetChannelsListAdapter
+import com.discord.widgets.channels.list.items.ChannelListItemPrivate
+import com.discord.widgets.channels.memberlist.adapter.ChannelMembersListAdapter
+import com.discord.widgets.channels.memberlist.adapter.ChannelMembersListViewHolderMember
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapter
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
+import com.discord.widgets.chat.list.entries.MessageEntry
+import com.discord.widgets.user.profile.UserProfileHeaderView
+import com.discord.widgets.user.profile.UserProfileHeaderViewModel
+
+abstract class Decorator {
+    open fun patch(patcher: PatcherAPI) {}
+
+    open fun onDMsInit(
+        holder: WidgetChannelsListAdapter.ItemChannelPrivate,
+        adapter: WidgetChannelsListAdapter
+    ) {}
+    open fun onDMsConfigure(
+        holder: WidgetChannelsListAdapter.ItemChannelPrivate,
+        item: ChannelListItemPrivate
+    ) {}
+
+    open fun onMembersListInit(
+        holder: ChannelMembersListViewHolderMember,
+        binding: WidgetChannelMembersListItemUserBinding
+    ) {}
+    open fun onMembersListConfigure(
+        holder: ChannelMembersListViewHolderMember,
+        item: ChannelMembersListAdapter.Item.Member,
+        adapter: ChannelMembersListAdapter
+    ) {}
+
+    open fun onMessageInit(
+        holder: WidgetChatListAdapterItemMessage,
+        adapter: WidgetChatListAdapter
+    ) {}
+    open fun onMessageConfigure(
+        holder: WidgetChatListAdapterItemMessage,
+        entry: MessageEntry
+    ) {}
+
+    open fun onProfileHeaderInit(view: UserProfileHeaderView) {}
+    open fun onProfileHeaderConfigure(
+        view: UserProfileHeaderView,
+        state: UserProfileHeaderViewModel.ViewState.Loaded
+    ) {}
+}

Aliucord/Aliucord

Aliucord/src/main/java/com/aliucord/coreplugins/Decorations.kt
@@ -1,37 +1,55 @@
 package com.aliucord.coreplugins
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.util.AttributeSet
 import com.aliucord.coreplugins.decorations.DecorationsSettings
+import com.aliucord.coreplugins.decorations.Decorator
 import com.aliucord.entities.CorePlugin
 import com.aliucord.patcher.*
 import com.aliucord.updater.ManagerBuild
 import com.aliucord.wrappers.users.*
 import com.discord.api.user.User
+import com.discord.databinding.WidgetChannelMembersListItemUserBinding
 import com.discord.models.member.GuildMember
 import com.discord.models.user.CoreUser
 import com.discord.models.user.MeUser
 import com.discord.stores.StoreGuilds
+import com.discord.widgets.channels.list.WidgetChannelsListAdapter
+import com.discord.widgets.channels.list.items.ChannelListItem
+import com.discord.widgets.channels.list.items.ChannelListItemPrivate
+import com.discord.widgets.channels.memberlist.adapter.*
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapter
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
+import com.discord.widgets.chat.list.entries.ChatListEntry
+import com.discord.widgets.chat.list.entries.MessageEntry
+import com.discord.widgets.user.profile.UserProfileHeaderView
+import com.discord.widgets.user.profile.UserProfileHeaderViewModel
 import com.discord.api.guildmember.GuildMember as ApiGuildMember
 
 internal class Decorations : CorePlugin(Manifest().apply {
     name = "Decorations"
     description = "Adds support for various user profile decorations"
 }) {
-    // TODO: make visible once plugin is ready
-    override val isHidden = true
-
     init {
         settingsTab = SettingsTab(DecorationsSettings.Sheet::class.java, SettingsTab.Type.BOTTOM_SHEET)
     }
 
+    @SuppressLint("BuildListAdds") // remove when there's stuff to add
+    @OptIn(ExperimentalStdlibApi::class)
+    private val decorators = buildList<Decorator> {
+        // if (DecorationsSettings.enableAvatarDecoration) add(AvatarDecorator())
+    }
+
     override fun start(context: Context) {
-        if (!DecorationsSettings.enable) return
         if (!ManagerBuild.hasInjector("2.3.0") || !ManagerBuild.hasPatches("1.3.0")) {
             logger.warn("Base app outdated, cannot enable Decorations")
             return
         }
 
         patchFields()
+        patchHandlers()
+        decorators.forEach { it.patch(patcher) }
     }
 
     override fun stop(context: Context) {
@@ -89,6 +107,74 @@ internal class Decorations : CorePlugin(Manifest().apply {
 
             res.avatarDecorationData = api.avatarDecorationData
             res.collectibles = api.collectibles
+        }
+    }
+
+    private fun patchHandlers() {
+        // onDMsInit
+        patcher.after<WidgetChannelsListAdapter.ItemChannelPrivate>(
+            Int::class.javaPrimitiveType!!,
+            WidgetChannelsListAdapter::class.java,
+        ) { (_, _: Int, adapter: WidgetChannelsListAdapter) ->
+            decorators.forEach { it.onDMsListInit(this, adapter) }
+        }
+
+        // onDMsConfigure
+        patcher.after<WidgetChannelsListAdapter.ItemChannelPrivate>(
+            "onConfigure",
+            Int::class.javaPrimitiveType!!,
+            ChannelListItem::class.java,
+        ) { (_, _: Int, item: ChannelListItemPrivate) ->
+            decorators.forEach { it.onDMsListConfigure(this, item) }
+        }
+
+        // onMembersListInit
+        patcher.after<ChannelMembersListViewHolderMember>(
+            WidgetChannelMembersListItemUserBinding::class.java,
+        ) { (_, binding: WidgetChannelMembersListItemUserBinding) ->
+            decorators.forEach { it.onMembersListInit(this, binding) }
+        }
+
+        // onMembersListConfigure
+        patcher.after<ChannelMembersListViewHolderMember>(
+            "bind",
+            ChannelMembersListAdapter.Item.Member::class.java,
+            Function0::class.java,
+        ) { (_, member: ChannelMembersListAdapter.Item.Member, callback: `ChannelMembersListAdapter$onBindViewHolder$1`) ->
+            decorators.forEach { it.onMembersListConfigure(this, member, callback.`this$0`) }
+        }
+
+        // onMessageInit
+        patcher.after<WidgetChatListAdapterItemMessage>(
+            Int::class.javaPrimitiveType!!,
+            WidgetChatListAdapter::class.java
+        ) { (_, _: Int, adapter: WidgetChatListAdapter) ->
+            decorators.forEach { it.onMessageInit(this, adapter) }
+        }
+
+        // onMessageConfigure
+        patcher.after<WidgetChatListAdapterItemMessage>(
+            "onConfigure",
+            Int::class.javaPrimitiveType!!,
+            ChatListEntry::class.java,
+        ) { (_, _: Int, entry: MessageEntry) ->
+            decorators.forEach { it.onMessageConfigure(this, entry) }
+        }
+
+        // onProfileHeaderInit
+        patcher.after<UserProfileHeaderView>(
+            Context::class.java,
+            AttributeSet::class.java,
+        ) {
+            decorators.forEach { it.onProfileHeaderInit(this) }
+        }
+
+        // onProfileHeaderConfigure
+        patcher.after<UserProfileHeaderView>(
+            "updateViewState",
+            UserProfileHeaderViewModel.ViewState.Loaded::class.java,
+        ) { (_, state: UserProfileHeaderViewModel.ViewState.Loaded) ->
+            decorators.forEach { it.onProfileHeaderConfigure(this, state) }
         }
     }
 }

Aliucord/src/main/java/com/aliucord/coreplugins/decorations/Decorator.kt
@@ -1,0 +1,51 @@
+package com.aliucord.coreplugins.decorations
+
+import com.aliucord.api.PatcherAPI
+import com.discord.databinding.WidgetChannelMembersListItemUserBinding
+import com.discord.widgets.channels.list.WidgetChannelsListAdapter
+import com.discord.widgets.channels.list.items.ChannelListItemPrivate
+import com.discord.widgets.channels.memberlist.adapter.ChannelMembersListAdapter
+import com.discord.widgets.channels.memberlist.adapter.ChannelMembersListViewHolderMember
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapter
+import com.discord.widgets.chat.list.adapter.WidgetChatListAdapterItemMessage
+import com.discord.widgets.chat.list.entries.MessageEntry
+import com.discord.widgets.user.profile.UserProfileHeaderView
+import com.discord.widgets.user.profile.UserProfileHeaderViewModel
+
+abstract class Decorator {
+    open fun patch(patcher: PatcherAPI) {}
+
+    open fun onDMsListInit(
+        holder: WidgetChannelsListAdapter.ItemChannelPrivate,
+        adapter: WidgetChannelsListAdapter
+    ) {}
+    open fun onDMsListConfigure(
+        holder: WidgetChannelsListAdapter.ItemChannelPrivate,
+        item: ChannelListItemPrivate
+    ) {}
+
+    open fun onMembersListInit(
+        holder: ChannelMembersListViewHolderMember,
+        binding: WidgetChannelMembersListItemUserBinding
+    ) {}
+    open fun onMembersListConfigure(
+        holder: ChannelMembersListViewHolderMember,
+        item: ChannelMembersListAdapter.Item.Member,
+        adapter: ChannelMembersListAdapter
+    ) {}
+
+    open fun onMessageInit(
+        holder: WidgetChatListAdapterItemMessage,
+        adapter: WidgetChatListAdapter
+    ) {}
+    open fun onMessageConfigure(
+        holder: WidgetChatListAdapterItemMessage,
+        entry: MessageEntry
+    ) {}
+
+    open fun onProfileHeaderInit(view: UserProfileHeaderView) {}
+    open fun onProfileHeaderConfigure(
+        view: UserProfileHeaderView,
+        state: UserProfileHeaderViewModel.ViewState.Loaded
+    ) {}
+}