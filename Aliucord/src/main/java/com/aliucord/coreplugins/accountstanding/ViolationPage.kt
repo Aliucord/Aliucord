b2f9f16855a26f0ab85509c98bd1b0f062804e54
Omar <om749843@gmail.com>
2025-11-08 21:40:10 +0200

    move the variables

--- Aliucord/src/main/java/com/aliucord/coreplugins/accountstanding/ViolationPage.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/accountstanding/ViolationPage.kt
@@ -23,13 +23,11 @@ import com.discord.utilities.time.TimeUtils
 import com.discord.utilities.SnowflakeUtils
 import com.discord.api.utcdatetime.UtcDateTime
 
-internal class ViolationPage(val violation: String, val flaggedContent: String?, val actions: List<String>, var id: Long, val maxExpirationTime: UtcDateTime) : SettingsPage() {
+internal class ViolationPage(val violation: String, val flaggedContent: String?, val actions: List<String>, var classificationId: Long, val maxExpirationTime: UtcDateTime) : SettingsPage() {
     override fun onViewBound(view: View) {
         super.onViewBound(view)
 
         val header = view.findViewById<ViewGroup>("action_bar_toolbar")
-        val occurredTime = TimeUtils.toReadableTimeString(context, SnowflakeUtils.toTimestamp(id), ClockFactory.get())
-        val expirationTime = TimeUtils.toReadableTimeString(context, SnowflakeUtils.toTimestamp(TimeUtils.millisToSnowflake(maxExpirationTime.g())), ClockFactory.get())
 
         // Remove the header title and description (since it's unneeded) and change the background color to the primary background color.
         header.removeViewAt(0)
@@ -55,6 +53,8 @@ internal class ViolationPage(val violation: String, val flaggedContent: String?,
         }.addTo(linearLayout)
 
         TextView(context, null, 0, R.i.UiKit_Settings_Item_SubText).apply {
+            val occurredTime = TimeUtils.toReadableTimeString(context, SnowflakeUtils.toTimestamp(classificationId), ClockFactory.get())
+            val expirationTime = TimeUtils.toReadableTimeString(context, SnowflakeUtils.toTimestamp(TimeUtils.millisToSnowflake(maxExpirationTime.g())), ClockFactory.get())
             text = "Violation will expire on $expirationTime\nOccurred on $occurredTime"
             textSize = 14f
             typeface = ResourcesCompat.getFont(context, Constants.Fonts.whitney_semibold)
