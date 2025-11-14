596772924e0732ac7804b2dca65572d2b3a60521
Cilly Leang <me@lava.moe>
2025-11-15 01:51:57 +0700

    feat(Decorations): implement avatar decorations (#638)
    * feat(Decorations): implement Decorator API
    
    * feat(Utils): add utilities to set padding more conveniently
    
    * feat(Decorations/Avatar): init

---
+++ Aliucord/src/main/java/com/aliucord/coreplugins/decorations/avatar/AvatarSticker.kt
@@ -0,0 +1,12 @@
+package com.aliucord.coreplugins.decorations.avatar
+
+import com.discord.api.sticker.*
+import com.discord.api.user.AvatarDecoration
+
+// We're using a StickerView for avatar decorations because it is the simplest way to get animated APNGs
+internal data class AvatarSticker(val data: AvatarDecoration) : BaseSticker {
+    override fun a(): StickerFormatType = StickerFormatType.APNG
+    override fun b(): String = ".png"
+    override fun c(): StickerPartial = throw UnsupportedOperationException("Unreachable") // This should never be called
+    override fun d(): Long = data.skuId
+}
