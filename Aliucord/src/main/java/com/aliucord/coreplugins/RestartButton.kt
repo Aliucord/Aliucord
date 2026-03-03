5588bebe4cb581d8d7a3448b995583d29ba30c23
Cilly Leang <mini@cilly.moe>
2026-03-03 17:28:45 +1100

    feat: add a "dump logs" button

--- Aliucord/src/main/java/com/aliucord/coreplugins/RestartButton.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/RestartButton.kt
@@ -3,12 +3,18 @@ package com.aliucord.coreplugins
 import android.content.Context
 import android.view.MenuItem
 import androidx.core.content.ContextCompat
+import com.aliucord.Constants
 import com.aliucord.Utils
 import com.aliucord.entities.CorePlugin
 import com.aliucord.patcher.after
 import com.discord.utilities.color.ColorCompat
 import com.discord.widgets.settings.WidgetSettings
 import com.lytefast.flexinput.R
+import java.io.File
+import java.io.InputStreamReader
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 internal class RestartButton : CorePlugin(Manifest("RestartButton")) {
     init {
@@ -29,6 +35,31 @@ internal class RestartButton : CorePlugin(Manifest("RestartButton")) {
                 .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS)
                 .setOnMenuItemClickListener {
                     Utils.restartAliucord(activity)
+                    false
+                }
+
+            val logIcon = ContextCompat.getDrawable(activity, R.e.ic_audit_logs_24dp)!!
+                .mutate()
+                .apply { setTint(ColorCompat.getThemedColor(activity, R.b.colorInteractiveNormal)) }
+
+            toolbar.menu.add("Dump logcat")
+                .setIcon(logIcon)
+                .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS)
+                .setOnMenuItemClickListener {
+                    val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmssSSS", Locale.getDefault())
+                    val time = dateFormat.format(Date())
+                    val dir = File(Constants.BASE_PATH, "logs")
+                    if (!dir.exists() && !dir.mkdir()) {
+                        Utils.showToast("Failed to create logs directory!", true)
+                    } else {
+                        val process = Runtime.getRuntime().exec(arrayOf("logcat", "-d"))
+                        Utils.threadPool.submit {
+                            val target = File(dir, "${time}.log")
+                            InputStreamReader(process.inputStream).copyTo(target.writer())
+                            Utils.showToast("Written to ${target.absolutePath}", true)
+                        }
+                    }
+
                     false
                 }
         }
