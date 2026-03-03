d2a7b2c4ca666bb306c7ad73363d3d97e3d4bf52
Cilly Leang <mini@cilly.moe>
2026-03-03 17:52:03 +1100

    fix: dump more logs

--- Aliucord/src/main/java/com/aliucord/coreplugins/RestartButton.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/RestartButton.kt
@@ -13,15 +13,29 @@ import com.lytefast.flexinput.R
 import java.io.File
 import java.io.InputStreamReader
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.util.*
 
 internal class RestartButton : CorePlugin(Manifest("RestartButton")) {
     init {
         manifest.description = "Adds a restart button for Aliucord to the settings page"
     }
 
+    var logs = ArrayDeque<String>(10000)
     override fun start(context: Context) {
+        val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmssSSS", Locale.getDefault())
+        val time = dateFormat.format(Date())
+        val process = Runtime.getRuntime().exec(arrayOf("logcat"))
+        Utils.threadPool.submit {
+            val reader = InputStreamReader(process.inputStream).buffered()
+            while (true) {
+                val nextLine = reader.readLine() ?: break
+                logs.add(nextLine)
+                if (logs.size > 10000) {
+                    logs.removeFirst()
+                }
+            }
+        }
+
         patcher.after<WidgetSettings>("configureToolbar") {
             val activity = this.requireAppActivity()
             val toolbar = activity.u
@@ -46,18 +60,14 @@ internal class RestartButton : CorePlugin(Manifest("RestartButton")) {
                 .setIcon(logIcon)
                 .setShowAsActionFlags(MenuItem.SHOW_AS_ACTION_ALWAYS)
                 .setOnMenuItemClickListener {
-                    val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmssSSS", Locale.getDefault())
-                    val time = dateFormat.format(Date())
                     val dir = File(Constants.BASE_PATH, "logs")
+                    val target = File(dir, "${time}.log")
+
                     if (!dir.exists() && !dir.mkdir()) {
                         Utils.showToast("Failed to create logs directory!", true)
                     } else {
-                        val process = Runtime.getRuntime().exec(arrayOf("logcat", "-d"))
-                        Utils.threadPool.submit {
-                            val target = File(dir, "${time}.log")
-                            InputStreamReader(process.inputStream).copyTo(target.writer())
-                            Utils.showToast("Written to ${target.absolutePath}", true)
-                        }
+                        target.writeText(logs.joinToString("\n"))
+                        Utils.showToast("Written to ${target.absolutePath}", true)
                     }
 
                     false
