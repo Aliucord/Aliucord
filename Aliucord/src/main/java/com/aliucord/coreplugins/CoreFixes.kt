65e5a47006e19ea00448fc955924520194549f85
canny1913 <canny1913@outlook.com>
2026-05-04 23:03:22 +0300

    chore: adhd

--- Aliucord/src/main/java/com/aliucord/coreplugins/CoreFixes.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/CoreFixes.kt
@@ -89,7 +89,7 @@ internal class CoreFixes : CorePlugin(Manifest("CoreFixes")) {
 
     private fun fixClock() = tryPatch("Fix Clock provider") {
         // Replace NTP clock with local system clock
-        // SAFETY: This is safe to run directly since ClockFactory initializes before Aliucord core.
+        // SAFETY: This is safe to run directly since ClockFactory initializes before Aliucord core initializes.
         val ntpClock = NtpClock(AndroidClock())
         ReflectUtils.setField(ClockFactory.INSTANCE, "ntpClock", ntpClock)
     }
