975e65ae1f24040d4c745ecd030724633f7fe3a7
Kofhisho <contact@alphexo.dev>
2026-06-24 00:30:30 +0100

    refactor(voice): Use correct `streams` type

--- Aliucord/src/main/java/com/aliucord/coreplugins/voice/NewIdentifyPayload.kt
+++ Aliucord/src/main/java/com/aliucord/coreplugins/voice/NewIdentifyPayload.kt
@@ -1,6 +1,7 @@
 package com.aliucord.coreplugins.voice
 
 import com.aliucord.utils.SerializedName
+import com.discord.rtcconnection.socket.io.Payloads
 
 data class NewIdentifyPayload(
     @SerializedName("server_id") val serverId: String,
@@ -8,6 +9,6 @@ data class NewIdentifyPayload(
     @SerializedName("session_id") val sessionId: String,
     val token: String,
     val video: Boolean,
-    val streams: List<Any>,
+    val streams: List<Payloads.Stream>,
     @SerializedName("max_dave_protocol_version") val maxDaveProtocolVersion: Int,
 )
